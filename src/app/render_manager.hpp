70e5af62d1673077a10ddba6229d12b8a74d8e43
Electron7-7 <teahaile2@teahaile.com>
2025-05-27 17:23:14 -0400

    feat(render manager): added the `RenderManager` class (plus the `CameraProperty` class) and implemented almost all its features, save the ones that reference currently non-existent classes and variables.

---
+++ src/app/render_manager.hpp
@@ -0,0 +1,70 @@
+#ifndef RENDER_MANAGER_H
+#define RENDER_MANAGER_H
+
+#include "manager.hpp"
+#include <glm/glm.hpp>
+#define GLM_ENABLE_EXPERIMENTAL
+#include <glm/gtc/quaternion.hpp>
+
+// Valve comment: "Physics property for entities"
+class CameraProperty
+{
+public:
+    CameraProperty();
+
+    void GetForward(glm::vec3* ForwardVector);
+
+    glm::vec3 origin;
+    glm::quat quaternion;
+    glm::vec3 velocity;
+    glm::vec3 angular_velocity;
+
+private:
+    friend class RenderManager;
+};
+
+class RenderManager : public Manager<>
+{
+public:
+    // Inherited from _Manager
+    virtual bool Init();
+    virtual LevelReturnValue_t LevelInit(bool IsFirstCall);
+    virtual void Update();
+    virtual LevelReturnValue_t LevelShutdown(bool IsFirstCall);
+    virtual void Shutdown();
+
+    // Property allocation
+    CameraProperty* CreateCameraProperty();
+    void DestroyCameraProperty(CameraProperty* Property);
+
+    void RenderWorldInrect(int X, int Y, int Width, int Height);
+    void RenderWorldFullscreen();
+
+private:
+    // Set up a projection matrix for a 90 degree fov
+    void SetupProjectionMatrix(int Width, int Height, float FOV);
+
+    // Set up an orthographic projection matrix
+    void SetupOrthoMatrix(int Width, int Height);
+
+    // Sets up the camera
+    void SetupCameraRenderState();
+
+    // Draws the world
+    void RenderWorld();
+
+    // Note: I'm not currently planning on implementing multiplayer, but idk, maybe following Source's source code will make multiplayer easier?
+    // Done completely client-side, want total smoothness, so simulate at render interval
+    void UpdateLocalPlayerCamera();
+
+    bool render_world_fullscreen;
+    int render_x;
+    int render_y;
+    int render_width;
+    int render_height;
+};
+
+// Singleton accessor
+extern RenderManager* global_render_manager;
+
+#endif // RENDER_MANAGER_H
