90e0fe84bee5441a2bbf2faf95b50b01e921661b
Electron77 <teahaile2@teahaile.com>
2025-06-06 13:52:30 -0400

    refactor(privatized members): Made the private member variables in `RenderContext` more private by removing 'friend class RenderManager' and writing simple setter functions for each variable

--- src/engine/rendering/render_context.hpp
+++ src/engine/rendering/render_context.hpp
@@ -11,9 +11,13 @@ public:
     glm::mat4 GetModelMatrix()      const { return model_matrix;      }
     glm::vec3 GetViewPosition()     const { return view_position;     }
 
-private:
-    friend class RenderManager;
+    void SetProjectionMatrix(glm::mat4 ProjectionMatrix) { projection_matrix = ProjectionMatrix; }
+    void SetViewMatrix(glm::mat4 ViewMatrix)             { view_matrix = ViewMatrix;             }
+    void SetModelMatrix(glm::mat4 ModelMatrix)           { model_matrix = ModelMatrix;           }
+    void SetViewPosition(glm::vec3 ViewPosition)         { view_position = ViewPosition;         }
+    void SetMesh(const Mesh& MeshToRender)               { mesh_to_render = MeshToRender;        }
 
+private:
     glm::mat4 projection_matrix;
     glm::mat4 view_matrix;
     glm::mat4 model_matrix;
