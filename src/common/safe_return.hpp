83a598690a7bf8eaf16c8639a7d115a554f9360c
Electron7-7 <julian@frattarola.net>
2025-08-10 15:37:32 -0400

    chore: Added more status codes and removed the 'SetStatus' function from 'SafeStatus'

--- src/common/safe_return.hpp
+++ src/common/safe_return.hpp
@@ -32,15 +32,15 @@ struct Status
     inline static SafeStatus NO_ERROR                               = SafeStatus( 0b00000000000, "NO_ERROR"                               );
     inline static SafeStatus ERROR_GENERIC                          = SafeStatus( 0b10000000000, "ERROR_GENERIC"                          );
     inline static SafeStatus ERROR_ALREADY_ACTIVE                   = SafeStatus( 0b01000000000, "ERROR_ALREADY_ACTIVE"                   );
-    inline static SafeStatus WindowingBackendWINDOW_CREATION_FAILED = SafeStatus( 0b00100000000, "WindowingBackendWINDOW_CREATION_FAILED" );
-    inline static SafeStatus EventQueueNOT_ENABLED                  = SafeStatus( 0b00010000000, "EventQueueNOT_ENABLED"                  );
-    inline static SafeStatus EventQueueNOT_PROCESSING_EVENTS        = SafeStatus( 0b00001000000, "EventQueueNOT_PROCESSING_EVENTS"        );
-    inline static SafeStatus EventQueueQUEUE_EMPTY                  = SafeStatus( 0b00000100000, "EventQueueQUEUE_EMPTY"                  );
-    inline static SafeStatus EventQueueINVALID_KEY_ID               = SafeStatus( 0b00000010000, "EventQueueINVALID_KEY_ID"               );
-    inline static SafeStatus CommandLineFALLBACK_FUNCTION_USED      = SafeStatus( 0b00000001000, "CommandLineFALLBACK_FUNCTION_USED"      );
-    inline static SafeStatus CommandLineINVALID_COMMAND             = SafeStatus( 0b00000000100, "CommandLineINVALID_COMMAND"             );
-    inline static SafeStatus InputManagerKEY_NOT_FOUND              = SafeStatus( 0b00000000010, "InputManagerKEY_NOT_FOUND"              );
-    inline static SafeStatus InputManagerKEY_IS_LOCKED              = SafeStatus( 0b00000000001, "InputManagerKEY_IS_LOCKED"              );
+    inline static SafeStatus ERROR_INVALID_KEY_ID                   = SafeStatus( 0b00100000000, "ERROR_INVALID_KEY_ID"                   );
+    inline static SafeStatus WindowingBackendWINDOW_CREATION_FAILED = SafeStatus( 0b00010000000, "WindowingBackendWINDOW_CREATION_FAILED" );
+    inline static SafeStatus EventQueueNOT_ENABLED                  = SafeStatus( 0b00001000000, "EventQueueNOT_ENABLED"                  );
+    inline static SafeStatus EventQueueNOT_PROCESSING_EVENTS        = SafeStatus( 0b00000100000, "EventQueueNOT_PROCESSING_EVENTS"        );
+    inline static SafeStatus EventQueueEMPTY                        = SafeStatus( 0b00000010000, "EventQueueEMPTY"                        );
+    inline static SafeStatus InputManagerKEY_NOT_FOUND              = SafeStatus( 0b00000001000, "InputManagerKEY_NOT_FOUND"              );
+    inline static SafeStatus InputManagerKEY_IS_LOCKED              = SafeStatus( 0b00000000100, "InputManagerKEY_IS_LOCKED"              );
+    inline static SafeStatus CommandLineINVALID_COMMAND             = SafeStatus( 0b00000000010, "CommandLineINVALID_COMMAND"             );
+    inline static SafeStatus KeyBindsKEY_HAS_NO_BINDS               = SafeStatus( 0b00000000001, "KeyBindsKEY_HAS_NO_BINDS"               );
 };
 
 template<typename T>
@@ -50,8 +50,6 @@ public:
     SafeReturn(T Data, SafeStatus ReturnStatus = Status::NO_ERROR)
     :_data(Data), _status(ReturnStatus)
     {}
-
-    void SetStatus(SafeStatus NewStatus) { _status = NewStatus; }
 
     const T& Data() const { return _data; }
     SafeStatus Status() const { return _status; }
