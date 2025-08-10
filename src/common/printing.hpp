7421d32e3d7c614220409e7aeb184e3fa0eb42d8
Electron7-7 <julian@frattarola.net>
2025-08-10 15:38:09 -0400

    feature/refactor(depending on how you look at it): Changed how colors are used; they're more verbose and less functional, yes, but now much easier to use directly.

--- src/common/printing.hpp
+++ src/common/printing.hpp
@@ -7,26 +7,57 @@
 struct Color
 {
     constexpr static const char* Reset   = "\x1b[0m";
-    constexpr static const char* Black   = "30";
-    constexpr static const char* Red     = "31";
-    constexpr static const char* Green   = "32";
-    constexpr static const char* Yellow  = "33";
-    constexpr static const char* Blue    = "34";
-    constexpr static const char* Magenta = "35";
-    constexpr static const char* Cyan    = "36";
-    constexpr static const char* White   = "37";
-    constexpr static const char* Default = "39";
+
+    struct Front
+    {
+        constexpr static const char* Black   = "\x1b[30m";
+        constexpr static const char* Red     = "\x1b[31m";
+        constexpr static const char* Green   = "\x1b[32m";
+        constexpr static const char* Yellow  = "\x1b[33m";
+        constexpr static const char* Blue    = "\x1b[34m";
+        constexpr static const char* Magenta = "\x1b[35m";
+        constexpr static const char* Cyan    = "\x1b[36m";
+        constexpr static const char* White   = "\x1b[37m";
+        constexpr static const char* Default = "\x1b[39m";
+
+        constexpr static const char* BlackBold   = "\x1b[1;30m";
+        constexpr static const char* RedBold     = "\x1b[1;31m";
+        constexpr static const char* GreenBold   = "\x1b[1;32m";
+        constexpr static const char* YellowBold  = "\x1b[1;33m";
+        constexpr static const char* BlueBold    = "\x1b[1;34m";
+        constexpr static const char* MagentaBold = "\x1b[1;35m";
+        constexpr static const char* CyanBold    = "\x1b[1;36m";
+        constexpr static const char* WhiteBold   = "\x1b[1;37m";
+        constexpr static const char* DefaultBold = "\x1b[1;39m";
+    };
+
+    struct Back
+    {
+        constexpr static const char* Black   = "\x1b[40m";
+        constexpr static const char* Red     = "\x1b[41m";
+        constexpr static const char* Green   = "\x1b[42m";
+        constexpr static const char* Yellow  = "\x1b[43m";
+        constexpr static const char* Blue    = "\x1b[44m";
+        constexpr static const char* Magenta = "\x1b[45m";
+        constexpr static const char* Cyan    = "\x1b[46m";
+        constexpr static const char* White   = "\x1b[47m";
+        constexpr static const char* Default = "\x1b[49m";
+
+        constexpr static const char* BlackBold   = "\x1b[2;40m";
+        constexpr static const char* RedBold     = "\x1b[2;41m";
+        constexpr static const char* GreenBold   = "\x1b[2;42m";
+        constexpr static const char* YellowBold  = "\x1b[2;43m";
+        constexpr static const char* BlueBold    = "\x1b[2;44m";
+        constexpr static const char* MagentaBold = "\x1b[2;45m";
+        constexpr static const char* CyanBold    = "\x1b[2;46m";
+        constexpr static const char* WhiteBold   = "\x1b[2;47m";
+        constexpr static const char* DefaultBold = "\x1b[2;49m";
+    };
 };
 
-constexpr std::string COLOR(const std::string& color)
-{ return "\x1b[" + color + "m"; }
-
-constexpr std::string COLOR_BOLD(const std::string& color)
-{ return "\x1b[1;" + color + "m"; }
-
-const std::string ERROR = std::string( COLOR_BOLD(Color::Red)     + "[ERROR]"                );
-const std::string WARN  = std::string( COLOR_BOLD(Color::Yellow)  + "[WARNING]"              );
-const std::string DEBUG = std::string( COLOR_BOLD(Color::Default) + "[DEBUG]" + Color::Reset );
+const std::string ERROR = Color::Front::RedBold     + std::string("[ERROR]")               ;
+const std::string WARN  = Color::Front::YellowBold  + std::string("[WARNING]")             ;
+const std::string DEBUG = Color::Front::DefaultBold + std::string("[DEBUG]") + Color::Reset;
 
 // Useful macros
 #define __LABELLED_PRINT(LABEL, FORMAT, ARGS...) std::println("{}" FORMAT "{}", LABEL, ARGS, Color::Reset);
