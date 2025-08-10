74ee20adc7a7f60d291028213819f75fddd70c1c
Electron7-7 <julian@frattarola.net>
2025-08-10 15:33:59 -0400

    refactor: Changed the old 'ConsoleCommand' object type to 'Command', a typedef alias of 'std::string'. Changed the comparison function accordingly.

--- src/input/event.hpp
+++ src/input/event.hpp
@@ -1,34 +1,35 @@
 #ifndef EVENT_H
 #define EVENT_H
 
-/*#include "common/time.hpp"
-#include "console/command.hpp"
-#include <string>
+#include "time.hpp"
+#include "commands/command_line.hpp"
+
+#include <format>
 
 struct Event
 {
 public:
-    Event(): _console_command(Commands::_NULL) {}
-    Event(const ConsoleCommand& Command, const double& CreationTime = CurrentTime()): _creation_time(CreationTime), _console_command(Command) {}
+    Event(): _console_command(CommandLine::cmd_Null) {}
+    Event(Command ConsoleCommand, const double& CreationTime = CurrentTime()): _creation_time(CreationTime), _console_command(ConsoleCommand) {}
     Event(const Event& CopyFrom): _creation_time(CopyFrom._creation_time), _console_command(CopyFrom._console_command) {}
 
     double GetCreationTime() const { return _creation_time; }
-    const ConsoleCommand& GetCommand() const { return _console_command; }
-    std::string EventLog() const { return std::string("EventLog - Creation time: " + std::to_string(_creation_time) + ", ConsoleCommand: \"" + _console_command.GetCommand() + "\""); }
+    std::string GetCommand() const { return _console_command; }
+    std::string EventLog() const { return std::format("EventLog - Creation time: {}, Console command: '{}'\n", _creation_time, _console_command); }
 
     constexpr bool operator==(const Event& other) const
     {
         return
         (
-            _console_command == other._console_command &&
-            _creation_time   == other._creation_time
+            !_console_command.compare(other._console_command) &&
+            _creation_time == other._creation_time
         );
     }
     constexpr bool operator!=(const Event& other) const { return !(*this == other); }
 
 private:
     double _creation_time = CurrentTime();
-    ConsoleCommand _console_command;
-};*/
+    Command _console_command = CommandLine::cmd_Null;
+};
 
 #endif // EVENT_H
