c3f44ac0b2fc230d46e398a8ebedd8f939ab86b8
Electron7-7 <26826460+Electron7-7@users.noreply.github.com>
2025-08-20 23:32:49 -0400

    refactor: Updated `Mesh`, removing the '_mesh_name' member and updating the 'GraphXMesh' files accordingly

--- src/rendering/mesh.cpp
+++ src/rendering/mesh.cpp
@@ -3,48 +3,48 @@
 
 Mesh::Mesh() = default;
 
-Mesh::Mesh(const std::string& init_name, const std::vector<Vertex>& init_vertices, const std::vector<Index>& init_indices)
-: _mesh_name(init_name), _face_indices(init_indices)
+Mesh::Mesh(const std::vector<Vertex>& init_vertices, const std::vector<Index>& init_indices)
+: m_FaceIndices(init_indices)
 {
     for(unsigned int i = 0 ; i < init_vertices.size() ; i++)
-    { _vertices[i] = init_vertices.at(i); }
+    { m_Vertices[i] = init_vertices.at(i); }
 }
 
-Mesh::Mesh(const std::string& init_name, const std::vector<Vertex>& init_vertices)
-: Mesh(init_name, init_vertices, std::vector<Index>{})
+Mesh::Mesh(const std::vector<Vertex>& init_vertices)
+: Mesh(init_vertices, std::vector<Index>{})
 {}
 
-Mesh::Mesh(const std::string& init_name, const std::vector<Vertex>& init_vertices, const std::vector<uintvec3>& init_faces)
-: Mesh(init_name, init_vertices)
+Mesh::Mesh(const std::vector<Vertex>& init_vertices, const std::vector<uintvec3>& init_faces)
+: Mesh(init_vertices)
 {
     for(uintvec3 face : init_faces)
-    { _face_indices.insert(_face_indices.end(), {face[0], face[1], face[2]}); }
+    { m_FaceIndices.insert(m_FaceIndices.end(), {face[0], face[1], face[2]}); }
 }
 
 void Mesh::AddVertex(Vertex vertex)
-{ _vertices[_vertices.size()] = vertex; }
+{ m_Vertices[m_Vertices.size()] = vertex; }
 
 void Mesh::AddIndex(Mesh::Index index)
-{ _face_indices.insert(_face_indices.end(), index); }
+{ m_FaceIndices.insert(m_FaceIndices.end(), index); }
 
 void Mesh::AddFace(uintvec3 face_indices)
-{ _face_indices.insert(_face_indices.end(), {face_indices[0], face_indices[1], face_indices[2]}); }
+{ m_FaceIndices.insert(m_FaceIndices.end(), {face_indices[0], face_indices[1], face_indices[2]}); }
 
 void Mesh::RemoveVertex(Mesh::Index index_of_vertex_to_remove)
 {
-    if(_vertices.contains(index_of_vertex_to_remove))
-    { _vertices.erase(index_of_vertex_to_remove); }
+    if(m_Vertices.contains(index_of_vertex_to_remove))
+    { m_Vertices.erase(index_of_vertex_to_remove); }
     else
     { PRINT_WARNING("Mesh::RemoveVertex(Index) - No index matching the argument was found in this mesh") }
 }
 
 void Mesh::RemoveVertex(Vertex vertex_to_remove) // Much less efficient, but more direct
 {
-    for(auto& [index, vertex] : _vertices)
+    for(auto& [index, vertex] : m_Vertices)
     {
         if(vertex == vertex_to_remove)
         {
-            _vertices.erase(index);
+            m_Vertices.erase(index);
             return;
        }
     }
@@ -52,13 +52,10 @@ void Mesh::RemoveVertex(Vertex vertex_to_remove) // Much less efficient, but mor
     PRINT_WARNING("Mesh::RemoveVertex(Vertex) - No vertex matching the argument was found in this mesh")
 }
 
-void Mesh::SetName(const std::string& name)
-{ _mesh_name = name; }
-
 std::vector<float> Mesh::GetVertexData() const
 {
     std::vector<float> all_vertex_data = {};
-    for(auto& [index, vertex] : _vertices)
+    for(auto& [index, vertex] : m_Vertices)
     {
         // FIXME: change hardcoded '11' to a variable that helps identify the vertex data stride
         std::array<float, 11> iterative_vertex_data = vertex.VertexData();
@@ -69,7 +66,4 @@ std::vector<float> Mesh::GetVertexData() const
 }
 
 std::vector<Mesh::Index> Mesh::GetIndices() const
-{ return _face_indices; }
-
-std::string Mesh::GetName() const
-{ return _mesh_name; }
+{ return m_FaceIndices; }
--- src/rendering/mesh.hpp
+++ src/rendering/mesh.hpp
@@ -7,7 +7,6 @@
 #include <glm/glm.hpp>
 #include <vector>
 #include <map>
-#include <string>
 
 struct Mesh
 {
@@ -15,25 +14,22 @@ public:
     typedef unsigned int Index;
 
     Mesh();
-    Mesh(const std::string& Name, const std::vector<Vertex>& Vertices);
-    Mesh(const std::string& Name, const std::vector<Vertex>& Vertices, const std::vector<Index>& Indices);
-    Mesh(const std::string& Name, const std::vector<Vertex>& Vertices, const std::vector<uintvec3>& Faces);
+    Mesh(const std::vector<Vertex>& Vertices);
+    Mesh(const std::vector<Vertex>& Vertices, const std::vector<Index>& Indices);
+    Mesh(const std::vector<Vertex>& Vertices, const std::vector<uintvec3>& Faces);
 
     void AddVertex(Vertex NewVertex);
     void RemoveVertex(Index IndexOfVertexToRemove);
     void RemoveVertex(Vertex VertexToRemove); // Much less efficient, but more direct
     void AddIndex(Index NewIndex);
     void AddFace(uintvec3 NewFaceIndices);
-    void SetName(const std::string& NewName);
 
     std::vector<float> GetVertexData() const;
     std::vector<Index> GetIndices() const;
-    std::string GetName() const;
 
 private:
-    std::string _mesh_name = "UntitledMesh";
-    std::map<Index, Vertex> _vertices = {};
-    std::vector<Index> _face_indices = {};
+    std::map<Index, Vertex> m_Vertices = {};
+    std::vector<Index> m_FaceIndices = {};
 };
 
 #endif // MESH_H
