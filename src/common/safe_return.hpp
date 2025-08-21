a4c037fd477ccf5ac0e9b8e65277540d6a2ceaef
Electron7-7 <26826460+Electron7-7@users.noreply.github.com>
2025-08-20 23:45:44 -0400

    refactor: Changed the status ids from binary to hexadecimal (removing the ability to use them as bitmasks)

--- src/common/safe_return.hpp
+++ src/common/safe_return.hpp
@@ -37,20 +37,24 @@ private:
 
 struct Status
 {
-    inline static SafeStatus NO_ERROR                               = SafeStatus( 0b000000000000, "NO_ERROR"                               );
-    inline static SafeStatus ERROR_GENERIC                          = SafeStatus( 0b100000000000, "ERROR_GENERIC"                          );
-    inline static SafeStatus ERROR_ALREADY_ACTIVE                   = SafeStatus( 0b010000000000, "ERROR_ALREADY_ACTIVE"                   );
-    inline static SafeStatus ERROR_INVALID_KEY_ID                   = SafeStatus( 0b001000000000, "ERROR_INVALID_KEY_ID"                   );
-    inline static SafeStatus WindowingBackendWINDOW_CREATION_FAILED = SafeStatus( 0b000100000000, "WindowingBackendWINDOW_CREATION_FAILED" );
-    inline static SafeStatus WindowingBackendGRAPHICS_INIT_FAILED   = SafeStatus( 0b000100000000, "WindowingBackendGRAPHICS_INIT_FAILED"   );
-    inline static SafeStatus EventQueueNOT_ENABLED                  = SafeStatus( 0b000010000000, "EventQueueNOT_ENABLED"                  );
-    inline static SafeStatus EventQueueNOT_PROCESSING_EVENTS        = SafeStatus( 0b000001000000, "EventQueueNOT_PROCESSING_EVENTS"        );
-    inline static SafeStatus EventQueueEMPTY                        = SafeStatus( 0b000000100000, "EventQueueEMPTY"                        );
-    inline static SafeStatus InputManagerKEY_NOT_FOUND              = SafeStatus( 0b000000010000, "InputManagerKEY_NOT_FOUND"              );
-    inline static SafeStatus InputManagerKEY_IS_LOCKED              = SafeStatus( 0b000000001000, "InputManagerKEY_IS_LOCKED"              );
-    inline static SafeStatus CommandLineINVALID_COMMAND             = SafeStatus( 0b000000000100, "CommandLineINVALID_COMMAND"             );
-    inline static SafeStatus KeyBindsKEY_HAS_NO_BINDS               = SafeStatus( 0b000000000010, "KeyBindsKEY_HAS_NO_BINDS"               );
-    inline static SafeStatus SettingsINVALID_SETTING_NAME           = SafeStatus( 0b000000000001, "SettingsINVALID_SETTING_NAME"           );
+    inline static SafeStatus NO_ERROR;
+    inline static SafeStatus ERROR_GENERIC                          = SafeStatus( 0x1,  "ERROR_GENERIC"                          );
+    inline static SafeStatus ERROR_ALREADY_ACTIVE                   = SafeStatus( 0x2,  "ERROR_ALREADY_ACTIVE"                   );
+    inline static SafeStatus ERROR_INVALID_KEY_ID                   = SafeStatus( 0x3,  "ERROR_INVALID_KEY_ID"                   );
+    inline static SafeStatus WindowingBackendWINDOW_CREATION_FAILED = SafeStatus( 0x4,  "WindowingBackendWINDOW_CREATION_FAILED" );
+    inline static SafeStatus WindowingBackendGRAPHICS_INIT_FAILED   = SafeStatus( 0x5,  "WindowingBackendGRAPHICS_INIT_FAILED"   );
+    inline static SafeStatus EventQueueNOT_ENABLED                  = SafeStatus( 0x6,  "EventQueueNOT_ENABLED"                  );
+    inline static SafeStatus EventQueueNOT_PROCESSING_EVENTS        = SafeStatus( 0x7,  "EventQueueNOT_PROCESSING_EVENTS"        );
+    inline static SafeStatus EventQueueEMPTY                        = SafeStatus( 0x8,  "EventQueueEMPTY"                        );
+    inline static SafeStatus InputManagerKEY_NOT_FOUND              = SafeStatus( 0x9,  "InputManagerKEY_NOT_FOUND"              );
+    inline static SafeStatus InputManagerKEY_IS_LOCKED              = SafeStatus( 0xA,  "InputManagerKEY_IS_LOCKED"              );
+    inline static SafeStatus CommandLineINVALID_COMMAND             = SafeStatus( 0xB,  "CommandLineINVALID_COMMAND"             );
+    inline static SafeStatus KeyBindsKEY_HAS_NO_BINDS               = SafeStatus( 0xC,  "KeyBindsKEY_HAS_NO_BINDS"               );
+    inline static SafeStatus SettingsINVALID_SETTING_NAME           = SafeStatus( 0xD,  "SettingsINVALID_SETTING_NAME"           );
+    inline static SafeStatus ResourceManagerFAILED_TO_LOAD_OBJ      = SafeStatus( 0xE,  "ResourceManagerFAILED_TO_LOAD_OBJ"      );
+    inline static SafeStatus TheatreParserFILE_DOES_NOT_EXIST       = SafeStatus( 0xF,  "TheatreParserFILE_DOES_NOT_EXIST"       );
+    inline static SafeStatus TheatreParserWRONG_FILE_EXTENSION      = SafeStatus( 0x10, "TheatreParserWRONG_FILE_EXTENSION"      );
+    inline static SafeStatus TheatreParserNO_THEATRE_LOADED         = SafeStatus( 0x11, "TheatreParserNO_THEATRE_LOADED"         );
 };
 
 template<typename T>
