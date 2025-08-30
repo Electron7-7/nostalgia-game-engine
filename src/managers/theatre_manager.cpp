f9748bdbabd4ca732e2763c0d3f15a92785aa181
Electron7-7 <26826460+Electron7-7@users.noreply.github.com>
2025-08-29 21:17:36 -0400

    feature: Removed the parsing functions from 'TheatreManager'; these will be assigned to 'TheatreParser'

--- src/managers/theatre_manager.cpp
+++ src/managers/theatre_manager.cpp
@@ -1,32 +1,25 @@
 #include "theatre_manager.hpp"
 #include "rendering/render_command.hpp"
-#include "theatre/theatre_interpreting.hpp"
+#include "theatre/theatre_parser.hpp"
 
 #include <vector>
 #include <glm/glm.hpp>
 
 using namespace ManagerEnums;
 
-static std::vector<RenderCommand> s_RenderCommandQueue = {};
-static std::vector<Thing*> s_ThingStorage = {};
-
 static TheatreManager s_TheatreManager;
 TheatreManager* g_pTheatreManager = &s_TheatreManager;
 
-// Console variables for setting the forward and backward speed of the camera (hl2_src/app/legion/worldmanager.cpp:24-25)
-
-TheatreManager::TheatreManager()
-{
-}
-
-TheatreManager::~TheatreManager()
-{
-}
+static std::vector<RenderCommand> s_RenderCommandQueue = {};
+static std::vector<Thing*> s_ThingStorage = {};
 
 ManagerEnums::TheatreReturnValue_t TheatreManager::TheatreInit(bool is_first_call)
 {
     if(!is_first_call)
     { return FINISHED; }
+
+    // if(!SafeStatus::PrintCheck(TheatreParser::))
+    // { return FUCKED; }
 
     CreateThings();
 
@@ -41,20 +34,6 @@ ManagerEnums::TheatreReturnValue_t TheatreManager::TheatreShutdown(bool is_first
     DestroyThings();
 
     return FINISHED;
-}
-
-void TheatreManager::LoadTheatreFromMemory(const Theatre& new_theatre)
-{ m_CurrentTheatre = new_theatre; }
-
-bool TheatreManager::try_LoadTheatre(const std::string& theatre_file)
-{
-    if(SafeStatus::PrintCheck(try_LoadTheatreFile(theatre_file)))
-    {
-        m_CurrentTheatre = InterpretTheatreFile();
-        return true;
-    }
-
-    return false;
 }
 
 void TheatreManager::CreateThings()
--- src/managers/theatre_manager.hpp
+++ src/managers/theatre_manager.hpp
@@ -2,28 +2,20 @@
 #define THEATRE_MANAGER_H
 
 #include "manager.hpp"
-#include "theatre/theatre.hpp"
 #include "things/actors/nostalgia_player.hpp"
-
-#include <string>
 
 class TheatreManager : public Manager
 {
 public:
-    TheatreManager();
-    ~TheatreManager();
     ManagerEnums::TheatreReturnValue_t TheatreInit(bool IsFirstCall);
     ManagerEnums::TheatreReturnValue_t TheatreShutdown(bool IsFirstCall);
-
-    bool try_LoadTheatre(const std::string& TheatreFile);
-    void LoadTheatreFromMemory(const Theatre& Theatre);
 
 private:
     void CreateThings();
     void DestroyThings();
+    bool InterpretTheatre();
 
     NostalgiaPlayer m_LocalPlayer;
-    Theatre m_CurrentTheatre;
 };
 
 extern TheatreManager* g_pTheatreManager;
