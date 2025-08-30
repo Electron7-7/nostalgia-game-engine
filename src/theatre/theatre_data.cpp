6998e1e5217bae454a69ae7f7087c45fa8ccbf88
Electron7-7 <26826460+Electron7-7@users.noreply.github.com>
2025-08-29 21:22:06 -0400

    feature: Created 'TheatreData', 'variable_t', 'data_t', 'VariableType', and 'StringifyEnum'
    'TheatreData' is the new struct that will hold all of a Theatre file's parsed data. That data will be contained within 'data_t' objects, who will store their variables as 'variable_t' objects. 'data_t' and 'variable_t' have comparison operators and string constructors for use within 'std::set' and lookup-by-name functionality.

---
+++ src/theatre/theatre_data.cpp
@@ -0,0 +1,146 @@
+#include "theatre_data.hpp"
+#include "theatre_parsing_lookups.hpp"
+
+// Variable
+
+const variable_t variable_t::undefined = variable_t();
+
+variable_t::variable_t() = default;
+
+variable_t::variable_t(const std::string& name, const std::string& value, const VariableType& type)
+: m_Name(name), m_Value(value), m_Type(type), m_Hash(g_StringHash(m_Name)) {}
+
+variable_t::variable_t(const std::string& name)
+: variable_t(name, "N/A", VariableType::Default)
+{}
+
+// TODO: Is this bad?
+void variable_t::clear()
+{ *this = variable_t(); }
+
+// Data
+
+data_t::data_t(const std::string& name)
+: m_Name(name), m_Hash(g_StringHash(m_Name))
+{}
+
+data_t::data_t() = default;
+
+short data_t::SetVariable(const std::string& name, const std::string& data, const VariableType& type)
+{
+    bool variable_exists = (m_Variables.find(name) != m_Variables.end());
+    if(variable_exists)
+        { m_Variables.erase(name); }
+
+    m_Variables.emplace(name, data, type);
+    return variable_exists;
+}
+
+SafeReturn<const variable_t&> data_t::try_GetVariable(const std::string& name)
+{
+    if(!m_Variables.contains(name))
+    { return SafeReturn<const variable_t&>(variable_t::undefined, Status::DataTypeINVALID_VARIABLE_NAME); }
+
+    if(m_Variables.find(name)->m_Value.empty())
+    { return SafeReturn<const variable_t&>(variable_t::undefined, Status::DataTypeEMPTY_VARIABLE); }
+
+    return SafeReturn<const variable_t&>(*m_Variables.find(name));
+}
+
+const std::string& data_t::GetName() const
+{ return m_Name; }
+
+void data_t::SetName(const std::string& name)
+{
+    m_Name = name;
+    m_Hash = g_StringHash(m_Name);
+}
+
+const std::string& data_t::GetType() const
+{ return m_Type; }
+
+void data_t::SetType(const std::string& type)
+{
+    m_Type = type;
+    m_Hash = g_StringHash(m_Name);
+}
+
+void data_t::clear()
+{
+    m_Variables.clear();
+    m_Name = "Untitled";
+    m_Type = "Unset";
+}
+
+// Theatre Data
+
+data_t TheatreData::s_SafeDataReturn = data_t();
+
+const std::set<data_t>& TheatreData::GetThings() const
+{ return m_Things; }
+
+const std::set<data_t>& TheatreData::GetResources() const
+{ return m_Resources; }
+
+std::set<data_t> TheatreData::GetMergedData() const
+{
+    std::set<data_t> combined_sets(m_Things.cbegin(), m_Things.cend());
+    combined_sets.insert(m_Resources.cbegin(), m_Resources.cend());
+    return combined_sets;
+}
+
+SafeReturn<data_t> TheatreData::try_GetData(const std::string& name) const
+{
+    SafeReturn get_data = try_GetThingData(name);
+
+    if(get_data.Status() != Status::NO_ERROR)
+        { get_data = try_GetResourceData(name); }
+
+    return get_data;
+}
+
+SafeReturn<data_t> TheatreData::try_GetThingData(const std::string& name) const
+{
+    auto is_thing = m_Things.find(name);
+    if(is_thing != m_Things.cend())
+        { return *is_thing; }
+
+    return SafeReturn(s_SafeDataReturn, Status::TheatreDataTypeINVALID_NAME);
+}
+
+SafeReturn<data_t> TheatreData::try_GetResourceData(const std::string& name) const
+{
+    auto is_resource = m_Resources.find(name);
+    if(is_resource != m_Resources.cend())
+        { return *is_resource; }
+
+    return SafeReturn(s_SafeDataReturn, Status::TheatreDataTypeINVALID_NAME);
+}
+
+SafeStatus TheatreData::AddData(const data_t& data)
+{
+    switch(GetBaseType(data.GetType()))
+    {
+    case BaseType::Unknown:
+        return Status::TheatreDataTypeINVALID_NAME;
+    case BaseType::Resource:
+        AddResourceData(data);
+        break;
+    case BaseType::Thing:
+        AddThingData(data);
+        break;
+    }
+    return Status::NO_ERROR;
+}
+
+void TheatreData::clear()
+{
+    m_Things.clear();
+    m_Resources.clear();
+}
+
+void TheatreData::AddThingData(const data_t& data)
+{ m_Things.insert(data); }
+
+void TheatreData::AddResourceData(const data_t& data)
+{ m_Resources.insert(data); }
---
+++ src/theatre/theatre_data.hpp
@@ -0,0 +1,139 @@
+#ifndef THEATRE_DATA_H
+#define THEATRE_DATA_H
+
+#include "safe_return.hpp"
+#include "hash.hpp"
+#include "glm/fwd.hpp"
+
+#include <set>
+#include <string>
+
+enum class VariableType
+{
+    Nothing, // This is only used by the parser
+    TheatreRef,
+    EngineRef,
+    Number,
+    Bool,
+    String,
+    Default = String,
+};
+
+constexpr std::string StringifyEnum(const VariableType& VariableTypeEnum)
+{
+    switch(VariableTypeEnum)
+    {
+    case VariableType::TheatreRef:
+        return "TheatreRef";
+    case VariableType::EngineRef:
+        return "EngineRef";
+    case VariableType::Number:
+        return "Number";
+    case VariableType::Bool:
+        return "Bool";
+    case VariableType::String:
+        return "String";
+    default:
+        return "Uhh... what?";
+    }
+}
+
+struct variable_t
+{
+public:
+    static const variable_t undefined;
+
+    variable_t(const std::string& Name, const std::string& Value, const VariableType& Type);
+    variable_t();
+
+    const bool operator<(const variable_t& Compare) const
+        { return m_Hash < Compare.m_Hash; }
+
+private:
+#ifdef DEBUGGING
+    friend void debug_PrintParsedTheatreData();
+#endif
+    friend struct data_t;
+    variable_t(const std::string& Name);
+
+    std::string  m_Name  = "Untitled";
+    std::string  m_Value = "N/A";
+    VariableType m_Type  = VariableType::Default;
+    size_t       m_Hash  = g_StringHash(m_Name);
+
+    void clear();
+};
+
+struct data_t
+{
+public:
+    data_t();
+
+    static constexpr short VARIABLE_SET   = (short)true;
+    static constexpr short VARIABLE_ADDED = (short)false;
+
+    short SetVariable(const std::string& Name, const std::string& Value, const VariableType& Type);
+    SafeReturn<const variable_t&> try_GetVariable(const std::string& Name);
+
+    const std::string& GetName() const;
+    void SetName(const std::string& Name);
+
+    const std::string& GetType() const;
+    void SetType(const std::string& TypeName);
+
+    void clear();
+
+    // Variable Interpreting Functions
+    template<typename T, bool is_numeric = std::is_arithmetic_v<T>>
+    T GetNumber(const std::string& VariableName);
+    template<> float GetNumber(const std::string& VariableName);
+    template<> int GetNumber(const std::string& VariableName);
+    template<> glm::vec3 GetNumber(const std::string& VariableName);
+    template<> glm::vec2 GetNumber(const std::string& VariableName);
+
+    bool GetBool(const std::string& VariableName);
+
+    const bool operator<(const data_t& Compare) const
+        { return m_Hash < Compare.m_Hash; }
+
+private:
+#ifdef DEBUGGING
+    friend void debug_PrintParsedTheatreData();
+#endif
+    friend struct TheatreData;
+    data_t(const std::string& Name);
+
+    std::set<variable_t> m_Variables = {}; // The string key is the variable's name
+    std::string m_Name = "Untitled";
+    std::string m_Type = "Unset";
+    size_t      m_Hash = g_StringHash(m_Name);
+};
+
+struct TheatreData
+{
+    std::string m_Name = "Untitled Theatre";
+    long m_Index = -1; // FIXME: Make this not a magic number
+
+    const std::set<data_t>& GetThings() const;
+    const std::set<data_t>& GetResources() const;
+    std::set<data_t> GetMergedData() const;
+
+    SafeReturn<data_t> try_GetData(const std::string& Name) const; // Used by the parser
+    SafeReturn<data_t> try_GetThingData(const std::string& ThingName) const;
+    SafeReturn<data_t> try_GetResourceData(const std::string& ResourceName) const;
+
+    SafeStatus AddData(const data_t& Data);
+
+    void clear();
+
+private:
+    static data_t s_SafeDataReturn;
+
+    std::set<data_t> m_Things;
+    std::set<data_t> m_Resources;
+
+    void AddThingData(const data_t& ThingData);
+    void AddResourceData(const data_t& ResourceData);
+};
+
+#endif // THEATRE_DATA_H
