74b7475bf3828c582808536254a5d244c377287a
Electron7-7 <26826460+Electron7-7@users.noreply.github.com>
2025-08-29 21:28:32 -0400

    feature: Added some items for debugging Theatre parsing
    These are only available when compiled with the 'DEBUGGING' preprocessor flag enabled, as the functions are extremely slow and inefficient. It also includes a way of setting a runtime, dynamic breakpoint in the debug window (as long as you set a breakpoint on the PRINT_DEBUG statement in 'theatre_parser.cpp')

--- src/testing_app/ui/imgui_debugger.cpp
+++ src/testing_app/ui/imgui_debugger.cpp
@@ -1,12 +1,43 @@
 #include "imgui_debugger.hpp"
 #include "managers/backend_manager.hpp"
-#include "thirdparty/DearImGui/imgui.h"
+// #include "managers/theatre_manager.hpp"
+#include "theatre/theatre_parser.hpp"
 #include "settings/settings.hpp"
+#include "thirdparty/DearImGui/imgui.h"
+#include "thirdparty/DearImGui/imgui_stdlib.h"
 
 using namespace ImGui;
 
 static imgui_Debugger s_Debugger;
 imgui_Debugger* g_pDebugger = &s_Debugger;
+
+#ifdef DEBUGGING
+
+// This is very inefficient and bad, but it's just for debugging and I don't want to waste time optimizing it
+// (but, yes, I *do* hate the copy-pasted code)
+void debug_PrintParsedTheatreData()
+{
+    PRINT_DEBUG("Parsed Theatre Data Printout:")
+
+    SafeReturn<TheatreData> try_data = TheatreParser::try_GetTheatreData();
+    if(!SafeStatus::PrintCheck(try_data.Status()))
+    { return; }
+
+    std::set<data_t> temp_data = try_data.Data().GetMergedData();
+
+    for(const auto& data : temp_data)
+    {
+        std::print("{} {}\n", data.GetType(), data.GetName());
+
+        const std::set<variable_t>& variables = data.m_Variables;
+        for(const auto& variable : variables)
+        { std::print("\t[{}] '{}': '{}' ('{}')\n", variable.m_Hash, variable.m_Name, variable.m_Value, StringifyEnum(variable.m_Type)); }
+
+        std::print("\n");
+    }
+}
+
+#endif // DEBUGGING
 
 bool imgui_Debugger::Init()
 {
@@ -29,9 +60,24 @@ void imgui_Debugger::Update()
 {
     Begin("Debug Window");
 
-    if(Button("Load Test Theatre"))
+#ifdef DEBUGGING
+    static std::string s_TheatreFilePath = "theatres/NostalgiaHelloWorld.nt";
+    InputInt("Parser Breakpoint Line#", &g_BreakOnLine);
+    InputInt("Parser Breakpoint Column#", &g_BreakOnColumn);
+    InputText("NostalgiaTheatre File Path", &s_TheatreFilePath);
+    if(Button("Parse NostalgiaTheatre File"))
     {
+        SafeStatus::PrintCheck(TheatreParser::try_LoadTheatreFromFile(s_TheatreFilePath));
+        SafeStatus::PrintCheck(TheatreParser::try_ParseTheatre());
+        debug_PrintParsedTheatreData();
     }
+#endif // DEBUGGING
+
+    // if(Button("Load Test Theatre"))
+    // {
+    //     if(g_pTheatreManager->try_LoadTheatre("theatres/0.HelloWorld.gt"))
+    //     { _Manager::StartNewTheatre(); }
+    // }
 
     if(Button("Exit Test Theatre"))
     { _Manager::ShutdownTheatre(); }
