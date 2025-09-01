c4c5084651f443e81977e3c798626e0020534a3e
Electron7-7 <26826460+Electron7-7@users.noreply.github.com>
2025-08-31 23:32:40 -0400

    refactor: Inverted the boolean check in `WAIT_FOR`, to better match its name
    Now, you are "waiting for" the desired expression.

--- src/common/time.hpp
+++ src/common/time.hpp
@@ -11,6 +11,9 @@ struct Time
 // FIXME: Put these in a namespace/struct
 void SetWaitStart();
 double GetWaitStart();
-#define WAIT_FOR(CONDITION, PATIENCE) SetWaitStart(); while(CONDITION || (Time::Current() - GetWaitStart()) < PATIENCE) {}
+// When CONDITION is true, or we've waited for PATIENCE amount of time, exit the while loop
+#define WAIT_FOR(CONDITION, PATIENCE) \
+SetWaitStart(); \
+while(!CONDITION || (Time::Current() - GetWaitStart()) < PATIENCE) {}
 
 #endif // TIME_H
--- src/input/event_queue.cpp
+++ src/input/event_queue.cpp
@@ -98,7 +98,7 @@ SafeStatus EventQueue::try_BeginProcessing()
     if(_active_queue.empty() || _last_processed_event_index >= _active_queue.size())
     { return Status::EventQueueEMPTY; }
 
-    WAIT_FOR(is_queueing_events, 0.3); // *stares at '_active_queue' reeeeeally hard*
+    WAIT_FOR(!is_queueing_events, 0.3); // *stares at '_active_queue' reeeeeally hard*
 
     _safe_queue.clear(); // Just to be, well, safe
 
@@ -162,10 +162,10 @@ unsigned int EventQueue::GetCurrentQueueSize()
 
 void EventQueue::ClearQueue()
 {
-    WAIT_FOR(is_queueing_events, 1.0) // _active_queue has one second before we flip the fuck out and kill it with hammers
+    WAIT_FOR(!is_queueing_events, 1.0) // _active_queue has one second before we flip the fuck out and kill it with hammers
     _active_queue.clear();
 
-    WAIT_FOR(is_processing_events, 5.0) // _safe_queue gets five whole seconds because it's earned more love
+    WAIT_FOR(!is_processing_events, 5.0) // _safe_queue gets five whole seconds because it's earned more love
     _safe_queue.clear();
 }
 
