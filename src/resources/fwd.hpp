33461ada6a16f272244d627bd4a5dfbe4c504da2
Electron7-7 <26826460+Electron7-7@users.noreply.github.com>
2025-09-08 22:53:30 -0400

    feature: Created header files for 'Thing' and 'Resource' forward declarations

---
+++ src/resources/fwd.hpp
@@ -0,0 +1,13 @@
+#ifndef RESOURCES_FWD_H
+#define RESOURCES_FWD_H
+
+typedef unsigned int rid_t;
+
+// Forward Declarations
+struct Resource;
+struct Mesh;
+struct Texture;
+struct MeshInstance;
+struct Material;
+
+#endif // RESOURCES_FWD_H
---
+++ src/things/fwd.hpp
@@ -0,0 +1,13 @@
+#ifndef THINGS_FWD_H
+#define THINGS_FWD_H
+
+typedef unsigned int tid_t;
+
+// Forward Declarations
+class Thing;
+class Thinker;
+class Actor;
+class NostalgiaPlayer;
+class PrototypeActor;
+
+#endif // THINGS_FWD_H
