33da244dc7ac11f681ede8d9e812727698b3f391
Electron7-7 <26826460+Electron7-7@users.noreply.github.com>
2025-09-15 20:57:40 -0400

    feature: Removed the "getter" functions from 'TheatreManager'

--- src/managers/theatre_manager.hpp
+++ src/managers/theatre_manager.hpp
@@ -2,11 +2,14 @@
 #define THEATRE_MANAGER_H
 
 #include "manager.hpp"
-#include "time.hpp"
+#include "debug.hpp"
 #include "things/thing.hpp"
 #include "things/fwd.hpp"
+#include "rendering/render_command.hpp"
 
 #include <memory>
+#include <map>
+#include <glm/glm.hpp>
 
 class TheatreManager : public Manager
 {
@@ -17,44 +20,21 @@ public:
     ManagerEnums::TheatreReturnValue_t TheatreInit(bool IsFirstCall);
     ManagerEnums::TheatreReturnValue_t TheatreShutdown(bool IsFirstCall);
 
-    // FIXME: Refactor this to be more efficient
-    template<IsThing T>
-    static std::shared_ptr<T> GetThing(id_t id)
-    {
-        WAIT(s_AreThingsLocked, 10.0f);
-        s_AreThingsLocked = true;
-
-        if(!s_Things.contains(id))
-        {
-            PRINT_WARNING("TheatreManager::GetThing - Bad ID: '{}'", id);
-            return std::make_shared<T>();
-        }
-
-        auto try_Downcast = std::dynamic_pointer_cast<T>(s_Things.at(id));
-        if(!try_Downcast)
-        {
-            PRINT_WARNING("'Thing' downcast attempted with a non-'Thing' class")
-            return std::make_shared<T>();
-        }
-
-        s_AreThingsLocked = false;
-        return try_Downcast;
-    }
-
-    static NostalgiaPlayer* GetLocalPlayer();
+    void RenderWorld();
 
     static bool try_DestroyThing(id_t ThingID);
+    static std::shared_ptr<NostalgiaPlayer> GetLocalPlayer();
 
-#ifdef DEBUGGING
-    static const std::map<id_t, std::shared_ptr<Thing>>& debug_GetThings();
-#endif // DEBUGGING
+    static std::shared_ptr<Thing>& GetThing(id_t ID);
+    static const std::shared_ptr<Thing>& cGetThing(id_t ID);
 
 private:
-    static bool s_AreThingsLocked;
+    DEBUG(friend class imgui_Debugger;)
     static std::map<id_t, std::shared_ptr<Thing>> s_Things;
 
     static void CreateObjects();
     static void DestroyObjects();
+    static void PushValidRenderCommand(const RenderCommand&);
 };
 
 extern TheatreManager* g_pTheatreManager;
