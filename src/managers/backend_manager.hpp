3b65b54331b0631aad268876befe23a3980dd3da
Electron7-7 <26826460+Electron7-7@users.noreply.github.com>
2025-09-15 20:47:04 -0400

    feature: 'backend_manager.hpp' now includes 'rendering/backends/backend.hpp'

--- src/managers/backend_manager.hpp
+++ src/managers/backend_manager.hpp
@@ -2,12 +2,10 @@
 #define BACKEND_MANAGER_H
 
 #include "managers/manager.hpp"
+#include "rendering/backends/backend.hpp"
 
 // TODO: Implement a way to select your preferred API (e.g: Vulkan, OpenGL, etc), as I can't hot-swap the rendering API
 // unless I were to write my own windowing library that could, somehow, support that (which is a fucking insane idea)
-
-class GraphicsBackend; // Forward Declaration
-class WindowingBackend; // Forward Declaration
 
 typedef int BackendID;
 
