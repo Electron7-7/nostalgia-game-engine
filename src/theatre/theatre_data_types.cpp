21375c6f2cdc7628a6f20e51bde079551fee0e34
Electron7-7 <26826460+Electron7-7@users.noreply.github.com>
2025-09-17 16:21:18 -0400

    fix: Same as ac54218adef3defb70f9a1b96251dc24d51848c3
    Smart pointers still require pass-by-reference in this case

--- src/theatre/theatre_data_types.cpp
+++ src/theatre/theatre_data_types.cpp
@@ -106,7 +106,7 @@ bool data_t::GetTheatreRef(unsigned int& real_variable, const std::string& varia
     return StringToNum<unsigned int>(real_variable, variable->m_Value);
 }
 
-bool data_t::GetFileData(std::shared_ptr<const FileData> real_variable, const std::string& variable_name) const
+bool data_t::GetFileData(std::shared_ptr<const FileData>& real_variable, const std::string& variable_name) const
 {
     EARLY_RETURN_MACRO(variable)
     return(ResourceData::GetData(real_variable, variable->m_Value));
