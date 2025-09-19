6c400baa7eb881896c0c260a540c78a097f0425c
Electron7-7 <26826460+Electron7-7@users.noreply.github.com>
2025-09-18 21:48:37 -0400

    fix: Changed FOV
    I forgot it's the opposite of what I'm used to seeing in games.

--- src/settings/player_settings.cpp
+++ src/settings/player_settings.cpp
@@ -1,5 +1,5 @@
 #include "settings.hpp"
 
-float Settings::Player::FOV = 90.0f;
+float Settings::Player::FOV = 45.0f;
 float Settings::Player::ViewCutoffNear = 0.01f;
 float Settings::Player::ViewCutoffFar  = 1000.0f;
