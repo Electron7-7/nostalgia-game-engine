23450c942aeed50f335752b7cd4dbdf7d2e86194
Electron7-7 <26826460+Electron7-7@users.noreply.github.com>
2025-09-19 23:18:31 -0400

    feature: Added a 'fwd.hpp' file for Theatre data

---
+++ src/theatre/fwd.hpp
@@ -0,0 +1,15 @@
+#ifndef THEATRE_FWD_H
+#define THEATRE_FWD_H
+
+// Data
+struct data_t;
+struct variable_t;
+struct TheatreData;
+
+// Enum
+enum class VariableType;
+
+// Parser
+struct TheatreParser;
+
+#endif // THEATRE_FWD_H
