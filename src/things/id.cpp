95cf4af148d9942737cfc8dcc41c162f62f356b1
Electron7-7 <26826460+Electron7-7@users.noreply.github.com>
2025-09-22 19:26:13 -0400

    feature: Added 'AddID', 'Contains', and 'EndOfReservedIDs' to 'ID'
    'AddID' attempts to add the passed ID to 's_ExistingIDs' and returns true if successful or false if unsuccessful.
    'Contains' is a wrapper for 's_ExistingIds.contains(id)'.
    'EndOfReservedIDs' is a `constexpr uint` that lets me define a range of numbers from 0 to 'EndOfReservedIDs' which the random generator will never pick.

--- src/things/id.cpp
+++ src/things/id.cpp
@@ -9,7 +9,7 @@ static std::set<size_t> s_ExistingIDs = {};
 id_t ID::GetNewID()
 {
     std::mt19937 engine(s_RandomDevice());
-    std::uniform_int_distribution<id_t> distribution(2);
+    std::uniform_int_distribution<id_t> distribution(EndOfReservedIDs + 1);
 
     id_t return_value = distribution(engine);
     while(s_ExistingIDs.contains(return_value))
@@ -22,5 +22,16 @@ id_t ID::GetNewID()
 void ID::ClearIDs()
 { s_ExistingIDs.clear(); }
 
+bool ID::AddID(id_t id)
+{
+    if(Contains(id))
+        { return false; }
+    s_ExistingIDs.insert(id);
+    return true;
+}
+
 bool ID::RemoveID(id_t id)
 { return(s_ExistingIDs.erase(id)); }
+
+bool ID::Contains(id_t id)
+{ return s_ExistingIDs.contains(id); }
--- src/things/id.hpp
+++ src/things/id.hpp
@@ -6,10 +6,13 @@ typedef unsigned int id_t;
 struct ID
 {
     static constexpr id_t None = 0;
+    static constexpr id_t EndOfReservedIDs = 10;
 
     static id_t GetNewID();
     static void ClearIDs();
+    static bool AddID(id_t ID);
     static bool RemoveID(id_t ID);
+    static bool Contains(id_t ID);
 };
 
 #endif // ID_H
