26dee12e34799df7671e8829e27c75160bb31d1c
Electron7-7 <26826460+Electron7-7@users.noreply.github.com>
2025-09-30 20:54:08 -0400

    feature: Implemented rudimentary player controls

--- src/things/actors/nostalgia_player.cpp
+++ src/things/actors/nostalgia_player.cpp
@@ -1,5 +1,8 @@
 #include "nostalgia_player.hpp"
 #include "theatre/data_t.hpp"
+#include "input/event.hpp"
+#include "settings/settings.hpp"
+#include "managers/backend_manager.hpp"
 
 void NostalgiaPlayer::SetupVariables(const data_t& data)
 {
@@ -14,6 +17,38 @@ bool NostalgiaPlayer::Initialize()
         { return false; }
     return true;
 }
+
+void NostalgiaPlayer::Input(const InputEvent& event)
+{
+    Move({
+        event.IsAction("+right")   - event.IsAction("+left"),
+        event.IsAction("+forward") - event.IsAction("+backward")
+    });
+    if(g_pBackendManager->Windowing()->GetMouseMode() != MouseMode::Disabled)
+        { m_LookWish = glm::vec2(0.0f); return; }
+    else if(event.IsType(InputEventType::MouseMotion))
+        { Look(event.MouseMotion()); }
+}
+
+void NostalgiaPlayer::Tick()
+{
+    glm::vec3 l_FrontBackVelocity = glm::vec3(Front()[0], 0.0f, Front()[2]) * (m_MovementDirection[1] * Settings::Player::MovementSpeed);
+    glm::vec3 l_LeftRightVelocity = Right() * (m_MovementDirection[0] * Settings::Player::MovementSpeed);
+    m_Velocity = (l_FrontBackVelocity + l_LeftRightVelocity);
+    m_Velocity[1] = 0.0f;
+    m_Origin += m_Velocity;
+    m_MovementDirection = glm::vec3(0.0f);
+    Euler(Euler(true) -= glm::vec3(m_LookWish.y, m_LookWish.x, 0.0f), true);
+}
+
+void NostalgiaPlayer::Move(const glm::vec2& direction)
+{
+    m_MovementDirection.x += direction.x;
+    m_MovementDirection.y += direction.y;
+}
+
+void NostalgiaPlayer::Look(const glm::vec2& motion)
+{ m_LookWish = motion * (Settings::Player::MouseSensitivity * Settings::Player::MouseSensitivityScale); }
 
 const glm::vec3& NostalgiaPlayer::ViewPosition() const
 { return m_ViewPosition; }
--- src/things/actors/nostalgia_player.hpp
+++ src/things/actors/nostalgia_player.hpp
@@ -2,10 +2,19 @@
 #define NOSTALGIA_PLAYER_H
 
 #include "actor.hpp"
+#include "debug.hpp"
 
 #include <glm/glm.hpp>
 #define GLM_ENABLE_EXPERIMENTAL
 #include <glm/gtc/quaternion.hpp>
+
+enum class Movement
+{
+    Forward,
+    Backward,
+    Left,
+    Right
+};
 
 // Actor type
 class NostalgiaPlayer : public Actor
@@ -13,13 +22,20 @@ class NostalgiaPlayer : public Actor
 public:
     virtual void SetupVariables(const data_t&);
     virtual bool Initialize();
+    virtual void Input(const InputEvent&);
+    virtual void Tick();
 
-    // View position is an offset from the player's origin
-    const glm::vec3& ViewPosition() const;
+    void Move(const glm::vec2& Direction);
+    void Look(const glm::vec2& Position);
+    const glm::vec3& ViewPosition() const; // View position is an offset from the player's origin
     void SetViewPosition(const glm::vec3& ViewPosition);
 
 private:
+    DEBUG(friend void l_TerribleRenderDebugWindow();)
+    glm::vec3 m_Velocity = glm::vec3(0.0f);
     glm::vec3 m_ViewPosition = glm::vec3(0.0f, 1.5f, 0.0f);
+    glm::vec2 m_MovementDirection = glm::vec2(0.0f);
+    glm::vec2 m_LookWish = glm::vec2(0.0f);
 };
 
 #endif // NOSTALGIA_PLAYER_H
