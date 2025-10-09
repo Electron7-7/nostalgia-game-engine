4a00d9d8b94e980639a3b6612606ecd4f91f867a
Electron7-7 <26826460+Electron7-7@users.noreply.github.com>
2025-10-09 14:50:44 -0400

    refactor(breaking): Redesigned printouts Printouts are now function templates that mimic their `std` counterparts. Debug printouts are either function templates or empty macros depending on if `DEBUGGING` is defined or not. An extra printout was added to the debug printouts for debug-only printouts without the '[DEBUG]' label (`print_debugging`).

--- src/common/printing.hpp
+++ src/common/printing.hpp
@@ -1,27 +1,38 @@
 #ifndef PRINTING_H
 #define PRINTING_H
 
-#include <print> // IWYU pragma: keep
+#include <print>
 
-// IMPORTANT - These labels do not set the text color back to normal! If you use them manually, make sure to do that!
-constexpr const char* __ERROR    = "\x1b[1;31m[ERROR]";   // Sets text color to "bold red"
-constexpr const char* __WARNING  = "\x1b[1;33m[WARNING]"; // Sets text color to "bold yellow"
-constexpr const char* __DEBUG    = "\x1b[1;36m[DEBUG]";   // Sets text color to "bold cyan"
-constexpr const char* __RESET_NL = "\x1b[0m\n";           // Reset all changes & add a newline
+template<class... Args>
+    void print_error(std::format_string<Args...> fmt, Args&&... args)
+    { std::println("\x1b[1;31m[ERROR] {}\x1b[0m",
+        std::format(fmt, std::forward<Args>(args)...)); }
 
-/*
-    My printout macros are inspired by these ingenious StackOverflow answers:
-        https://stackoverflow.com/a/3048361  - Original Concept
-        https://stackoverflow.com/a/8814003  - Slight Improvement w/ Explanation
-        https://stackoverflow.com/a/28074198 - Major Improvement w/ Explanation
+template<class... Args>
+    void print_warning(std::format_string<Args...> fmt, Args&&... args)
+    { std::println("\x1b[1;33m[WARNING] {}\x1b[0m",
+            std::format(fmt, std::forward<Args>(args)...)); }
 
-    I give my "greatest respects" to Derek Ledbetter, David Sorkovsky, Syphorlate, Jens Gustedt, and Jason Deng.
-    Even if I didn't end up using their work, it was a great inspiration and a really good read.
-*/
+// Debug printouts are only enabled in the Debug build
+#ifdef DEBUGGING
+template<class... Args>
+    void print_debug(std::format_string<Args...> fmt, Args&&... args)
+    { std::println("\x1b[1;36m[DEBUG] {}\x1b[0m",
+            std::format(fmt, std::forward<Args>(args)...)); }
 
-#define __LABELLED_PRINT(LABEL, FORMAT, ...) std::print("{} " FORMAT "{}", LABEL __VA_OPT__(,) __VA_ARGS__, __RESET_NL);
+template<class... Args>
+    void print_debugging(std::format_string<Args...> fmt, Args&&... args)
+    { std::println(fmt, std::forward<Args>(args)...); }
 
-#define PRINT_ERROR(FORMAT, ARGS...)   __LABELLED_PRINT(__ERROR, FORMAT, ARGS)
-#define PRINT_WARNING(FORMAT, ARGS...) __LABELLED_PRINT(__WARNING,  FORMAT, ARGS)
+template<class... Args>
+    void print_app_debug(std::format_string<Args...> fmt, Args&&... args)
+    { std::println("\x1b[1;35m[APP DEBUG] {}\x1b[0m",
+            std::format(fmt, std::forward<Args>(args)...)); }
+#else  // !DEBUGGING
+// The curly brackets help avoid some syntax errors
+#   define print_debug(FMT,ARGS...)     {}
+#   define print_debugging(FMT,ARGS...) {}
+#   define print_app_debug(FMT,ARGS...) {}
+#endif // DEBUGGING
 
 #endif // PRINTING_H
