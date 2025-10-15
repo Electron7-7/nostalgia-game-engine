6bd9beeb0245f6719b6f055b55cbbf7265d2eab0
Electron7-7 <26826460+Electron7-7@users.noreply.github.com>
2025-10-15 18:13:42 -0400

    refactor+feature(printing): changed formatting of code and added a `print_jolt_debug` function

--- src/common/printing.hpp
+++ src/common/printing.hpp
@@ -4,21 +4,18 @@
 #include <print>
 
 template<class... Args>
-    void print_error(std::format_string<Args...> fmt, Args&&... args)
-    { std::println("\x1b[1;31m[ERROR] {}\x1b[0m",
-        std::format(fmt, std::forward<Args>(args)...)); }
+    bool print_error(std::format_string<Args...> fmt, Args&&... args)
+    { std::println("\x1b[1;31m[ERROR] {}\x1b[0m", std::format(fmt, std::forward<Args>(args)...)); return false; }
 
 template<class... Args>
     void print_warning(std::format_string<Args...> fmt, Args&&... args)
-    { std::println("\x1b[1;33m[WARNING] {}\x1b[0m",
-            std::format(fmt, std::forward<Args>(args)...)); }
+    { std::println("\x1b[1;33m[WARNING] {}\x1b[0m", std::format(fmt, std::forward<Args>(args)...)); }
 
 // Debug printouts are only enabled in the Debug build
 #ifdef DEBUGGING
 template<class... Args>
     void print_debug(std::format_string<Args...> fmt, Args&&... args)
-    { std::println("\x1b[1;36m[DEBUG] {}\x1b[0m",
-            std::format(fmt, std::forward<Args>(args)...)); }
+    { std::println("\x1b[1;36m[DEBUG] {}\x1b[0m", std::format(fmt, std::forward<Args>(args)...)); }
 
 template<class... Args>
     void print_debugging(std::format_string<Args...> fmt, Args&&... args)
@@ -26,13 +23,18 @@ template<class... Args>
 
 template<class... Args>
     void print_app_debug(std::format_string<Args...> fmt, Args&&... args)
-    { std::println("\x1b[1;35m[APP DEBUG] {}\x1b[0m",
-            std::format(fmt, std::forward<Args>(args)...)); }
+    { std::println("\x1b[1;35m[APP DEBUG] {}\x1b[0m", std::format(fmt, std::forward<Args>(args)...)); }
+
+template<class... Args>
+    void print_jolt_debug(std::format_string<Args...> fmt, Args&&... args)
+    {   std::println("\x1b[1;32m[JOLT] {}\x1b[0m", std::format(fmt, std::forward<Args>(args)...)); }
+
 #else  // !DEBUGGING
 // The curly brackets help avoid some syntax errors
 #   define print_debug(FMT,ARGS...)     {}
 #   define print_debugging(FMT,ARGS...) {}
 #   define print_app_debug(FMT,ARGS...) {}
+#   define print_jolt_debug(FMT,ARGS...) {}
 #endif // DEBUGGING
 
 #endif // PRINTING_H
