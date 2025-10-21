f52769cd29cd3668f69e6e6b3a6c1bb96f92dca4
Electron7-7 <26826460+Electron7-7@users.noreply.github.com>
2025-10-21 16:52:10 -0400

    feature(fwd): added a forward-declarations header for backends

---
+++ src/backends/fwd.hpp
@@ -0,0 +1,15 @@
+#ifndef BACKENDS_FWD_H
+#define BACKENDS_FWD_H
+
+// Base Backend Classes
+class _Backend;
+class WindowingBackend;
+class GraphicsBackend;
+
+// Windowing Specializations
+class GLFW_Backend;
+
+// Graphics Specializations
+class OpenGL_Backend;
+
+#endif // BACKENDS_FWD_H
