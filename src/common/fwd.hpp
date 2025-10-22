b535b195dc46bf344498874bf2b0fdaed90a3490
Electron7-7 <26826460+Electron7-7@users.noreply.github.com>
2025-10-21 22:34:54 -0400

    feature: added a forward declarations file for 'src/common'

---
+++ src/common/fwd.hpp
@@ -0,0 +1,17 @@
+#ifndef COMMON_FWD_H
+#define COMMON_FWD_H
+
+// globals.hpp
+extern const char* gTrue;
+extern const char* gFalse;
+
+// ids.hpp
+struct ID;
+
+// penum_t.hpp
+struct penum_t;
+
+// safe_return.hpp
+struct SafeStatus;
+
+#endif // COMMON_FWD_H
