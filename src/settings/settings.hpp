331bd4199b0d2c6a43297d3850aed57fcd083574
Electron7-7 <26826460+Electron7-7@users.noreply.github.com>
2025-10-21 22:35:28 -0400

    refactor: removed "custom settings" code; you can just add your settings to the namespace(s)

--- src/settings/settings.hpp
+++ src/settings/settings.hpp
@@ -1,26 +1,12 @@
 #ifndef SETTINGS_H
 #define SETTINGS_H
 
-#include "common/safe_return.hpp"
+#include "common/fwd.hpp"
 
 #include <glm/fwd.hpp>
 
 namespace Settings
 {
-    // The 'SetSetting' and 'try_Get-' functions are for users to define their own settings without needing to recompile
-    // the library. However, there will also be structs full of static settings that can't be removed; these are settings
-    // that are more universal, like window width/height, certain graphics options, and the tick rate. Use the functions
-    // (and, consequently, the maps) when adding custom options to the engine/game. Use the static variables when their
-    // specific purpose is required (e.g: setting the window's fullscreen state).
-
-    extern void SetSetting(const char* SettingName, const char* Setting);
-    extern void SetSetting(const char* SettingName, float Setting);
-    extern void SetSetting(const char* SettingName, int Setting);
-
-    extern SafeReturn<const char*> try_GetString(const char* SettingName);
-    extern SafeReturn<float>       try_GetFloat(const char* SettingName);
-    extern SafeReturn<int>         try_GetInt(const char* SettingName);
-
     namespace Engine
     {
         extern int   TickRate;
