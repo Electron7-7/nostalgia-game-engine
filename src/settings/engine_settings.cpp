d7fca3c52f1aa411cf1ddfd7c0670263d1cad90e
Electron7-7 <26826460+Electron7-7@users.noreply.github.com>
2025-10-22 17:51:09 -0400

    style(assignments)

--- src/settings/engine_settings.cpp
+++ src/settings/engine_settings.cpp
@@ -1,7 +1,7 @@
 #include "settings.hpp"
 #include "backends/backend.hpp"
 
-int   Settings::Engine::TickRate = 70;
+int   Settings::Engine::TickRate{70};
 float Settings::Engine::TickInterval() { return (1.0f / TickRate); }
 ID    Settings::Engine::GraphicsBackend{BackendIDs::gOpenGL};
 ID    Settings::Engine::WindowingBackend{BackendIDs::wGLFW};
--- src/settings/player_settings.cpp
+++ src/settings/player_settings.cpp
@@ -1,10 +1,10 @@
 #include "settings.hpp"
 
 // TODO: Replace `Settings::Player::FOV` with horizontal and/or vertical FOVs? (currently it's only vertical)
-float Settings::Player::FOV = 45.0f;
-float Settings::Player::ViewCutoffNear = 0.01f;
-float Settings::Player::ViewCutoffFar  = 1000.0f;
-bool  Settings::Player::RawMouseMotion = true;
-float Settings::Player::MouseSensitivity = 3.5f;
-float Settings::Player::MouseSensitivityScale = 0.01f;
-float Settings::Player::MovementSpeed = 0.4f;
+float Settings::Player::FOV{45.0f};
+float Settings::Player::ViewCutoffNear{0.01f};
+float Settings::Player::ViewCutoffFar{1000.0f};
+bool  Settings::Player::RawMouseMotion{true};
+float Settings::Player::MouseSensitivity{3.5f};
+float Settings::Player::MouseSensitivityScale{0.01f};
+float Settings::Player::MovementSpeed{0.4f};
