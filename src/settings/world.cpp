7157164858ec7fd76f9d62b734135c72a1873813
Electron7-7 <26826460+Electron7-7@users.noreply.github.com>
2025-10-28 16:32:38 -0400

    fix(forgot to add 'settings/world.cpp')

---
+++ src/settings/world.cpp
@@ -0,0 +1,32 @@
+#include "world.hpp"
+#include "backends/backend.hpp"
+
+#include <glm/glm.hpp>
+#define GLM_ENABLE_EXPERIMENTAL
+#include <glm/gtc/quaternion.hpp>
+
+using namespace Settings;
+
+static glm::vec3 sOrientationUp{0.0f, 1.0f, 0.0f};
+static glm::vec3 sOrientationRight{1.0f, 0.0f, 0.0f};
+static glm::vec3 sOrientationFront{0.0f, 0.0f, -1.0f};
+
+const glm::vec3& World::Up()
+{ return sOrientationUp; }
+
+const glm::vec3& World::Right()
+{ return sOrientationRight; }
+
+const glm::vec3& World::Front()
+{ return sOrientationFront; }
+
+bool World::UpdateOrientation(const ID& id)
+{
+    switch((uint)id)
+    {
+    case BackendIDs::gOpenGL():
+        return true; // Orientation is set to OpenGL standard by default
+    default:
+        return false;
+    }
+}
