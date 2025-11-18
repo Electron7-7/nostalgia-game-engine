a2774b836a90602c2bb6870b842f81a8cd3beb39
Electron7-7 <26826460+Electron7-7@users.noreply.github.com>
2025-11-18 16:26:30 -0500

    features(core): a new 'Error' enum, stolen from Godot, and helper macros for structs and function arguments The macros specifically are for quickly defining a struct + "argument" typedef, and shortening certain function arguments.

---
+++ Engine/src/core/error.hpp
@@ -0,0 +1,17 @@
+#ifndef ERROR_H
+#define ERROR_H
+
+// Modelled after GDScript's `Error` enum:
+// https://docs.godotengine.org/en/stable/classes/class_%40globalscope.html#enum-globalscope-error
+enum Error
+{
+    NOT_IMPLEMENTED = -1, // Unimplemented code; not techincally an error but not successful, either
+    OK = 1, // Successfull
+    FAILED = 0, // Generic Error
+    ERR_INIT_FAILED, // Failure during initialization of some object
+    ERR_SWITCH_DEFAULT, // A switch statement's `default` case was met (and is considered an error)
+    ERR_INDEX_OUT_OF_BOUNDS,
+    ERR_NOT_FOUND, // Search failure
+};
+
+#endif // ERROR_H
