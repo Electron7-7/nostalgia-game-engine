9abf0714c3becc1c35eb39e985eb53b808f5badb
Electron7-7 <26826460+Electron7-7@users.noreply.github.com>
2025-11-21 16:16:20 -0500

    feature+refactor: `Unique` and `Shared` are stolen from Hazel's 'Scope' and 'Ref' templates Also, changed `FARG` to not take a variable name.

--- Engine/src/core/type_helpers.hpp
+++ Engine/src/core/type_helpers.hpp
@@ -1,12 +1,30 @@
 #ifndef TYPE_HELPERS_H
 #define TYPE_HELPERS_H
 
+#include <memory>
+
 #define MAKE_TYPE_ARG(STRUCT_NAME, STRUCT_DEFINITION, OPTIONAL_CSTYLE_VARIABLE) \
 struct STRUCT_NAME { \
     STRUCT_DEFINITION \
 } OPTIONAL_CSTYLE_VARIABLE; \
 typedef const STRUCT_NAME& STRUCT_NAME##Arg;
 
-#define FARG(TYPE, NAME) const TYPE& NAME
+#define FARG(TYPE) const TYPE&
+
+// Stolen from Hazel
+template<typename T>
+    using Unique = std::unique_ptr<T>;
+
+template<typename T, class ...Args>
+    constexpr Unique<T> MakeUnique(Args&&... args)
+    { return std::make_unique<T>(std::forward<Args>(args)...); }
+
+// Stolen from Hazel
+template<typename T>
+    using Shared = std::shared_ptr<T>;
+
+template<typename T, class ...Args>
+    constexpr Unique<T> MakeShared(Args&&... args)
+    { return std::make_shared<T>(std::forward<Args>(args)...); }
 
 #endif // TYPE_HELPERS_H
