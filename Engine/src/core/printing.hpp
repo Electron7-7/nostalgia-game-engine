7c36ec5c1ed2da0e17ed2a2f1e4ca4a1c23f1a4d
Electron7-7 <26826460+Electron7-7@users.noreply.github.com>
2025-11-21 19:22:43 -0500

    feature/overhaul(printing): major re-work of how printing works + added verbosity Printing is now done via pre-processor definitions again, because verbosity is now added. A print statement's verbosity can be between 0-3 and controls how much the `std::source_location` is used in the print statement.

--- Engine/src/core/printing.hpp
+++ Engine/src/core/printing.hpp
@@ -1,42 +1,123 @@
 #ifndef PRINTING_H
 #define PRINTING_H
 
-// NOTE: The `bool` return is a way of returning Error::OK/Error::FAILED without including the header file.
+#include "error.hpp"
 
 #include <print>
+#include <source_location>
+
+enum VERBOSITY {
+    VERBOSE0 = 0,
+    VERBOSE1 = 1,
+    VERBOSE2 = 2,
+    VERBOSE3 = 3,
+};
+
+inline constinit const char* __no_label{""};
+inline constinit const char* __error_label{"\x1b[91m[ERROR] "};
+inline constinit const char* __warning_label{"\x1b[38;5;220m[WARNING] "};
 
 template<class... Args>
-    bool print_error(std::format_string<Args...> fmt, Args&&... args)
-    { std::println("\x1b[1;31m[ERROR] {}\x1b[0m", std::format(fmt, std::forward<Args>(args)...)); return false; }
+    bool __print_verbose(bool return_value, VERBOSITY verbosity, std::format_string<Args...> fmt, const std::source_location loc, const char* label = "", Args&&... args)
+    {
+        if(verbosity <= 0)
+            { std::println("\x1b[1m{}\x1b[22m{}\x1b[0m", label, std::format(fmt, std::forward<Args>(args)...)); return return_value; }
+        std::string location{label};
+        std::string pieces[3] {
+            std::format("file {}", loc.file_name()),
+            std::format("({}:{})", loc.line(), loc.column()),
+            std::format(" `{}`", loc.function_name()),
+        };
+        for(int i{0}; i < verbosity; ++i)
+            { location += pieces[i]; }
+        if(fmt.get().empty())
+            { std::println("\x1b[1m{}\x1b[0m", location); }
+        else
+            { std::println("\x1b[1m{}: \x1b[22m{}\x1b[0m", location, std::format(fmt, std::forward<Args>(args)...)); }
+        return return_value;
+    }
 
-template<class... Args>
-    bool print_warning(std::format_string<Args...> fmt, Args&&... args)
-    { std::println("\x1b[1;33m[WARNING] {}\x1b[0m", std::format(fmt, std::forward<Args>(args)...)); return false; }
+#define print_verbose(Verbosity, Format, Args...) \
+    __print_verbose(true, Verbosity, Format, std::source_location::current(), __no_label, ## Args);
+
+// Always returns false. Use with: bad behaviour that leads to a crash/failure
+#define print_error(Format, Args...) \
+    __print_verbose(false, VERBOSE3, fmt, std::source_location::current(), __error_label, ## Args);
+
+// Always returns true. Use with: bad behaviour that doesn't lead to a crash/failure (not great, not terrible)
+#define print_warning(Format, Args...) \
+    __print_verbose(true, VERBOSE2, fmt, std::source_location::current(), __warning_label, ## Args);
 
 // Debug printouts are only enabled in the Debug build
 #ifdef DEBUGGING
-template<class... Args>
-    void print_debug(std::format_string<Args...> fmt, Args&&... args)
-    { std::println("\x1b[1;36m[DEBUG] {}\x1b[0m", std::format(fmt, std::forward<Args>(args)...)); }
+    inline constinit const char* __debug_label{"\x1b[1;36m[DEBUG] "};
+    inline constinit const char* __app_label{"\x1b[1;35m[APPLICATION] "};
+    inline constinit const char* __jolt_label{"\x1b[1;32m[JOLT] "};
 
-template<class... Args>
-    void print_debugging(std::format_string<Args...> fmt, Args&&... args)
-    { std::println(fmt, std::forward<Args>(args)...); }
+//  Debug message that starts with the '[DEBUG]' label
+#   define print_debug(fmt, args...) \
+        __print_verbose(true, VERBOSE0, fmt, std::source_location::current(), __debug_label, ## args)
 
-template<class... Args>
-    void print_app_debug(std::format_string<Args...> fmt, Args&&... args)
-    { std::println("\x1b[1;35m[APP DEBUG] {}\x1b[0m", std::format(fmt, std::forward<Args>(args)...)); }
+//  Verbose debug message that starts with the '[DEBUG]' label
+#   define print_debugv(verbosity, fmt, args...) \
+        __print_verbose(true, verbosity, fmt, std::source_location::current(), __debug_label, ## args)
 
-template<class... Args>
-    void print_jolt_debug(std::format_string<Args...> fmt, Args&&... args)
-    {   std::println("\x1b[1;32m[JOLT] {}\x1b[0m", std::format(fmt, std::forward<Args>(args)...)); }
+//  When `DEBUGGING` is undefined this will be empty, keeping debug string literals out of the release binary
+#   define debug_print(fmt, args...) std::println(fmt, ## args);
+
+//  Debug message that starts with the '[APPLICATION]' label
+#   define print_app(fmt, args...) \
+        __print_verbose(true, VERBOSE0, fmt, std::source_location::current(), __app_label, ## args)
+
+//  Verbose debug message that starts with the '[APPLICATION]' label
+#   define print_appv(verbosity, fmt, args...) \
+        __print_verbose(true, verbosity, fmt, std::source_location::current(), __app_label, ## args)
+
+//  Debug message that starts with the '[JOLT]' label
+#   define print_jolt(fmt, args...) \
+        __print_verbose(true, VERBOSE0, fmt, std::source_location::current(), __jolt_label, ## args)
+
+//  Verbose debug message that starts with the '[JOLT]' label
+#   define print_joltv(verbosity, fmt, args...) \
+        __print_verbose(true, verbosity, fmt, std::source_location::current(), __jolt_label, ## args)
 
 #else  // !DEBUGGING
-// The curly brackets help avoid some syntax errors
-#   define print_debug(FMT,ARGS...)     {}
-#   define print_debugging(FMT,ARGS...) {}
-#   define print_app_debug(FMT,ARGS...) {}
-#   define print_jolt_debug(FMT,ARGS...) {}
+    consteval bool __print_function_disabled(bool ret = true) { return ret; }
+#   define print_debug(...)  __print_function_disabled();
+#   define print_debugv(...) __print_function_disabled();
+#   define debug_print(...)  // See line 63
+#   define print_app(...)    __print_function_disabled();
+#   define print_appv(...)   __print_function_disabled();
+#   define print_jolt(...)   __print_function_disabled();
+#   define print_joltv(...)  __print_function_disabled();
 #endif // DEBUGGING
+
+inline Error print_error_enum(Error error, bool unimplemented_returns_ok = true, const std::source_location location = std::source_location::current())
+{
+#   define Case(ERROR, MESSAGE, OTHER...) case ERROR: err_msg = #ERROR" (" MESSAGE ")"; OTHER break;
+    std::string err_msg{""};
+    switch(error)
+    {
+    default: error = FAILED; err_msg = "well, congrats, you've managed to royally fuck up the error-printing function (you probably just passed it a negative integer instead of an Error, or some shit like that)"; break;
+    case OK: return OK;
+    Case(UNIMPLEMENTED, "some functionality that hasn't been implemented yet")
+    Case(FAILED, "unspecified error")
+    Case(ERR_INIT_FAILED, "failure during object initialization. Look for any `::Init` or `::Initialize` class methods")
+    Case(ERR_INIT_PARTIALLY_FAILED, "partial failure during object initialization. Look for any `::Init` or `::Initialize` class methods")
+    Case(ERR_SWITCH_DEFAULT, "a `switch` statement's `default:` case was met and is considered an error")
+    Case(ERR_INDEX_OUT_OF_BOUNDS, "generic 'index out of bounds' error")
+    Case(ERR_NOT_FOUND, "generic search error")
+    Case(ERR_FILE, "generic file error")
+    Case(ERR_FILE_LOAD, "`FileData::LoadFile` error")
+    Case(ERR_FILE_READ, "file read error")
+    Case(ERR_FILE_WRITE, "file write error")
+    Case(ERR_FILE_READ_WRITE, "file read/write error")
+    }
+    __print_verbose(false, VERBOSE3, "{}", location,
+        (error == UNIMPLEMENTED && unimplemented_returns_ok) ? __warning_label : __error_label,
+        err_msg);
+    return (error == UNIMPLEMENTED && unimplemented_returns_ok) ? OK : error;
+#   undef Case
+}
 
 #endif // PRINTING_H
