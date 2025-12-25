b3e029d1445ddc3e50805d5e53f15add92d8df95
Electron7-7 <26826460+Electron7-7@users.noreply.github.com>
2025-12-24 23:09:58 -0500

    feature: started work on `Camera3D` and `EditorCamera3D` classes

---
+++ Engine/src/things/actors/camera_3d.cpp
@@ -0,0 +1,46 @@
+#include "camera_3d.hpp"
+#include "theatre/transform_3d.hpp"
+#include "theatre/parser/thing_data.hpp"
+
+//////////////
+// Camera3D //
+//////////////
+void Camera3D::SetVariables(Farg<ThingData> data)
+{
+    Actor::SetVariables(data);
+
+    data.GetVariable(mCurrent, "Current", "CurrentCamera", "IsCurrent");
+}
+
+Shared<ThingData> Camera3D::GetVariables() const
+{
+    Shared<ThingData> data{Actor::GetVariables()};
+
+    data->AddVariable(mCurrent, "Current");
+
+    return data;
+}
+
+void Camera3D::SetRenderLayers(Farg<RenderLayers> inLayers)
+{ mRenderLayers = inLayers; }
+
+Farg<RenderLayers> Camera3D::GetRenderLayers() const
+{ return mRenderLayers; }
+
+void Camera3D::SetCurrent(bool isCurrent)
+{ mCurrent = isCurrent; }
+
+bool Camera3D::IsCurrent() const
+{ return mCurrent; }
+
+////////////////////
+// EditorCamera3D //
+////////////////////
+void EditorCamera3D::SetVariables(Farg<ThingData> data)
+{ Camera3D::SetVariables(data); }
+
+Shared<ThingData> EditorCamera3D::GetVariables() const
+{
+    Shared<ThingData> data{Camera3D::GetVariables()};
+    return data;
+}
---
+++ Engine/src/things/actors/camera_3d.hpp
@@ -0,0 +1,41 @@
+#ifdef FWD_DCL
+class Camera3D;
+class EditorCamera3D;
+#elif !defined CAMERA_3D_H
+#define CAMERA_3D_H
+
+#include "actor.hpp"
+#include "rendering/render_layers.hpp"
+
+class Camera3D : public Actor
+{
+public:
+    virtual void SetVariables(Farg<ThingData>) override;
+    virtual Shared<ThingData> GetVariables() const override;
+
+    virtual void SetRenderLayers(Farg<RenderLayers> inLayers);
+    virtual Farg<RenderLayers> GetRenderLayers() const;
+
+    virtual void SetCurrent(bool isCurrent);
+    virtual bool IsCurrent() const;
+
+    Farg<glm::mat4> ViewMatrix() const;
+
+protected:
+    RenderLayers mRenderLayers{};
+    bool mCurrent{false};
+};
+
+class EditorCamera3D : public Camera3D
+{
+public:
+    void SetVariables(Farg<ThingData>) final;
+    Shared<ThingData> GetVariables() const final;
+
+    void SetRenderLayers(Farg<RenderLayers>) final {}
+
+    void SetCurrent(bool) final {}
+    bool IsCurrent() const final { return true; }
+};
+
+#endif // CAMERA_3D_H
