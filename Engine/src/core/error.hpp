0f58bbbc43f4530c29cd7f4429f6ccf76462d0bc
Electron7-7 <26826460+Electron7-7@users.noreply.github.com>
2026-01-01 16:28:58 -0500

    feature(Error): added more `Error` enums and moved them around a bit

--- Engine/src/core/error.hpp
+++ Engine/src/core/error.hpp
@@ -9,23 +9,25 @@ enum Error : int {
     OK = 0, // Successful
     UNIMPLEMENTED = -1, // Unimplemented code; `TestError` can take an argument which decides if this is an error or not
     FAILED = 1, // Generic/default error
-    ERR_INVALID_ID, // Invalid `ID` error
-    ERR_INVALID_TYPE, // Invalid type error
     ERR_INIT_FAILED, // Total failure during initialization
     ERR_INIT_PARTIALLY_FAILED, // Partial failure during initialization
     ERR_SWITCH_DEFAULT, // A switch statement's `default` case was met (and is considered an error)
     ERR_INDEX_OUT_OF_BOUNDS,
     ERR_NOT_FOUND, // Generic search failure
-    ERR_ALREADY_EXISTS, // Example use: InputManager::AddAction returns this if the given "action name" already exists in the unordered map
-    ERR_NOT_ALLOWED, // Example use: OpenGLRendererAPI::RemoveViewport returns this if the user tries to remove the default "main window" viewport
-    ERR_EMPTY, // Some container is empty
-    ERR_FULL, // Some container is full
     ERR_FILE, // Generic file error
     ERR_FILE_LOAD, // Specifically, a `FileData::LoadFile` error
     ERR_FILE_READ,
     ERR_FILE_WRITE,
     ERR_FILE_READ_WRITE,
     ERR_DATA_LOAD, // Example use: OpenGLTextureBuffer's constructor sets `mStatus` to this if `stbi_load_from_memory` fails
+    ERR_NOT_LOADED,
+    ERR_INVALID_ID, // Invalid `ID` error
+    ERR_INVALID_TYPE, // Invalid type error
+    ERR_ALREADY_EXISTS, // Example use: InputManager::AddAction returns this if the given "action name" already exists in the unordered map
+    ERR_NOT_ALLOWED, // Example use: OpenGLRendererAPI::RemoveViewport returns this if the user tries to remove the default "main window" viewport
+    ERR_EMPTY, // Some container is empty
+    ERR_FULL, // Some container is full
+    ERR_NULLPTR, // A pointer is `nullptr`
 };
 
 constexpr bool operator!(const Error& inError) noexcept
