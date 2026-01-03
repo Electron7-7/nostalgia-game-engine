c6febbb4b80a676e4fb1070a9dc5c941f3d12073
Electron7-7 <26826460+Electron7-7@users.noreply.github.com>
2026-01-02 22:55:32 -0500

    feature(Thing): implemented children This time, they work.

--- Engine/src/things/thing.cpp
+++ Engine/src/things/thing.cpp
@@ -7,17 +7,35 @@ Thing::Thing()  noexcept = default;
 Thing::~Thing() noexcept = default;
 
 void Thing::Free()
-{ g_pTheatreManager->DestroyThing(mUID); }
+{
+    LockGuard<RMutex> lock{mChildrenMutex};
+    for(auto child : mChildren)
+        { if(child.is_unique) { g_pTheatreManager->GetThing(child.id)->unset_parent(); } }
+    g_pTheatreManager->GetThing(mParentID)->remove_child(mUID);
+    g_pTheatreManager->DestroyThing(mUID);
+}
+
+void Thing::Ready()
+{
+    LockGuard<RMutex> lock{mChildrenMutex};
+    for(auto child : mChildren)
+        { if(child.is_unique) { g_pTheatreManager->GetThing(child.id)->set_parent(mUID); } }
+}
 
 void Thing::SetVariables(Farg<ThingData> data)
 {
     mUID  = data.uid;
     mName = data.name;
     mType = data.type();
+    data.GetChildren(mChildren);
 }
 
 Shared<ThingData> Thing::GetVariables() const
-{ return MakeShared<ThingData>(mName, mType, mUID); }
+{
+    auto data{MakeShared<ThingData>(mName, mType, mUID)};
+    data->SetChildren(mChildren);
+    return data;
+}
 
 ID Thing::uid() const
 { return mUID; }
@@ -46,3 +64,65 @@ bool Thing::uid(ID inID)
 
 void Thing::name(Sarg inName)
 { mName = inName; }
+
+Farg<children_t> Thing::children() const
+{ return mChildren; }
+
+ID Thing::parent() const
+{ return mParentID; }
+
+Error Thing::add_child(ThingChild inChild)
+{
+    LockGuard<RMutex> lock{mChildrenMutex};
+    if(inChild.id.invalid())
+        { return ERR_INVALID_ID; }
+    else if(auto found_it{std::find(mChildren.cbegin(), mChildren.cend(), inChild)};
+        inChild.is_unique and found_it != mChildren.cend())
+        { return ERR_ALREADY_EXISTS; }
+    mChildren.emplace_back(inChild);
+    if(inChild.is_unique)
+        { g_pTheatreManager->GetThing(inChild.id)->set_parent(mUID); }
+    return OK;
+}
+
+Error Thing::remove_child(ThingChild inChild)
+{
+    LockGuard<RMutex> lock{mChildrenMutex};
+    if(inChild.id.invalid())
+        { return ERR_INVALID_ID; }
+    else if(auto found_it{std::find(mChildren.cbegin(), mChildren.cend(), inChild)};
+        found_it != mChildren.cend())
+    {
+        if(found_it->is_unique)
+            { g_pTheatreManager->GetThing(found_it->id)->unset_parent(); }
+        mChildren.erase(found_it);
+        return OK;
+    }
+    return ERR_NOT_FOUND;
+}
+
+Error Thing::swap_child(ThingChild inChild, ThingChild inNewChild)
+{
+    if(Error status{add_child(inNewChild)}; !status)
+        { return status; }
+    else if(Error status{remove_child(inChild)}; !status)
+        { remove_child(inNewChild); return status; }
+    return OK;
+}
+
+Error Thing::set_parent(ID inID)
+{
+    LockGuard<RMutex> lock{mChildrenMutex};
+    g_pTheatreManager->GetThing(mParentID)->remove_child(mUID);
+    mParentID = inID;
+    g_pTheatreManager->GetThing(mParentID)->add_child(mUID);
+    return OK;
+}
+
+Error Thing::unset_parent()
+{
+    LockGuard<RMutex> lock{mChildrenMutex};
+    auto parent{g_pTheatreManager->GetThing(mParentID)};
+    parent->remove_child(mUID);
+    return set_parent(parent->mParentID);
+}
--- Engine/src/things/thing.hpp
+++ Engine/src/things/thing.hpp
@@ -5,16 +5,31 @@
 #include "fwd/core.hpp"
 #include "things/types.hpp"
 #include "core/smart_pointers.hpp"
+#include "core/mutex.hpp"
 #include "components/game_loop.hpp"
 #include "components/event_handling.hpp"
+#include <vector>
+
+struct ThingChild
+{
+    ThingChild() = default;
+    ThingChild(ID inID, bool isUnique = false):
+        id{inID}, is_unique{isUnique} {}
+    ID id{};
+    bool is_unique{false};
+    constexpr bool operator==(Farg<ThingChild> other) const noexcept
+    { return id == other.id and is_unique == other.is_unique; }
+};
+using children_t = std::vector<ThingChild>;
 
 class Thing : public OnInput, public OnTick, public OnUpdate
 {
 public:
+
     Thing() noexcept;
     virtual ~Thing() noexcept;
 
-    virtual void Ready() {}
+    virtual void Ready();
     virtual void Shutdown() {}
     virtual void Tick() override {}
     virtual void Update() override {}
@@ -38,10 +53,23 @@ public:
     const char* const c_name() const;
     Farg<TTID> type() const;
 
+    Farg<children_t> children() const;
+    ID parent() const;
+
+    Error add_child(ThingChild);
+    Error remove_child(ThingChild);
+    Error swap_child(ThingChild, ThingChild);
+
+    Error set_parent(ID);
+    Error unset_parent();
+
 protected:
     ID mUID{};
     std::string mName{"Untitled Thing"};
     TTID mType{"Thing"};
+    RMutex mChildrenMutex{};
+    ID mParentID{};
+    children_t mChildren{};
 };
 
 template<typename T>
