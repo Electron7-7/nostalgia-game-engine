637dad4d4e11c39695c115422ca52e3e4a26ff4a
Electron7-7 <26826460+Electron7-7@users.noreply.github.com>
2026-01-09 22:02:51 -0500

    refactor(mutex): changed `LockGuard` to be `const std::lock_guard`

--- Engine/src/core/mutex.hpp
+++ Engine/src/core/mutex.hpp
@@ -6,7 +6,7 @@
 using Mutex  = std::mutex;
 using RMutex = std::recursive_mutex;
 template<typename T>
-    using LockGuard = std::lock_guard<T>;
+    using LockGuard  = const std::lock_guard<T>;
 template<typename T>
     using ScopedLock = std::scoped_lock<T>;
 
