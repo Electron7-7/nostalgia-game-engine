53359765519c4c78dc88899f60b9576c66f09d1e
Electron7-7 <26826460+Electron7-7@users.noreply.github.com>
2026-01-09 21:04:07 -0500

    feature(Settings::Graphics): added the clear color to the graphics settings

--- Engine/src/settings/graphics.hpp
+++ Engine/src/settings/graphics.hpp
@@ -1,11 +1,14 @@
 #ifndef GRAPHICS_SETTINGS_H
 #define GRAPHICS_SETTINGS_H
 
+#include "fwd/math.hpp"
+
 namespace Settings
 {
     namespace Graphics
     {
         extern bool GlobalWireframe;
+        extern ColorRGBA ClearColor;
     }
 }
 
