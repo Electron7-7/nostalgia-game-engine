dd479e9e765d2ef24a6751347d792db92203fe52
Electron7-7 <26826460+Electron7-7@users.noreply.github.com>
2026-01-26 18:17:21 -0500

    feature(Settings::Graphics): implemented quick stretch modes for rendering This will be implemented similarly to how Godot does it.

--- Engine/src/settings/graphics.hpp
+++ Engine/src/settings/graphics.hpp
@@ -5,9 +5,17 @@ namespace Settings
 {
     namespace Graphics
     {
+        enum class StretchMode   { Disabled, Viewport };
+        enum class StretchAspect { Ignore, Keep, KeepWidth, KeepHeight, };
 
         inline bool GlobalWireframe{false};
         inline ColorRGBA ClearColor{0.29f, 0.34f, 0.26f, 1.0f};
+
+        namespace Stretch
+        {
+            inline StretchMode Mode{StretchMode::Disabled};
+            inline StretchAspect Aspect{StretchAspect::Ignore};
+        }
     }
 }
 
