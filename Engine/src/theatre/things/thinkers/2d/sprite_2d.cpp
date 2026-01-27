aa296be8c6e306a09a77d1ce3e0ab9e318523d0d
Electron7-7 <26826460+Electron7-7@users.noreply.github.com>
2026-01-27 17:49:46 -0500

    feature(TestAnimatedSprite2D): implemented a tester Sprite2D object that cycles its texture ID

--- Engine/src/theatre/things/thinkers/2d/sprite_2d.cpp
+++ Engine/src/theatre/things/thinkers/2d/sprite_2d.cpp
@@ -1,4 +1,5 @@
 #include "sprite_2d.hpp"
+#include "managers/manager.hpp"
 
 using namespace TheatreFile;
 
@@ -31,3 +32,44 @@ bool Sprite2D::Wireframe() const
 
 void Sprite2D::SetWireframe(bool inWireframe)
 { mWireframe = inWireframe; }
+
+void TestAnimatedSprite2D::SetVariables(Farg<ThingData> data)
+{
+    Sprite2D::SetVariables(data);
+
+    data.get_variable(mInterval, "Interval");
+
+    int i{0};
+    ID id{};
+    while(data.get_variable(id, std::format("Texture{}", i)) == OK)
+    {
+        mTextureIDs.push_back(id);
+        ++i;
+    }
+    if(!mTextureIDs.empty())
+        { mTextureID = mTextureIDs.front(); }
+}
+
+Shared<ThingData> TestAnimatedSprite2D::GetVariables() const
+{
+    auto data{Sprite2D::GetVariables()};
+
+    data->set_variable(mInterval, "Interval");
+
+    int i{0};
+    for(ID id : mTextureIDs)
+        { data->set_variable(id, std::format("Texture{}", i)); }
+
+    return data;
+}
+
+void TestAnimatedSprite2D::Tick()
+{
+    if(!(Manager::TickNumber() % mInterval))
+    {
+        mIndex = (++mIndex >= mTextureIDs.size())
+            ? 0
+            : mIndex;
+        mTextureID = mTextureIDs[mIndex];
+    }
+}
