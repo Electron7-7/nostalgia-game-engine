6e4263954379284246a5f35b20a84e3d53f75676
Electron7-7 <26826460+Electron7-7@users.noreply.github.com>
2026-02-06 21:36:26 -0500

    feature(Console): implemented a console `Console` is able to store string commands and `Console::Variable` objects, which pair a string name with a string, int, and float values.

---
+++ Engine/Nostalgia/console/console.cpp
@@ -0,0 +1,204 @@
+#include "./console.hpp"
+#include "thirdparty/frozen/set.h"
+
+using namespace Console;
+
+static ConsoleCommandCallback_f sCommandCallback{nullptr};
+static Shared<Variable> sEmptyVariable{MakeShared<Variable>()};
+static constexpr frozen::set<char, 4> sWhitespace{' ', '\t', '\n', '\r'};
+static Commands
+sCommands{
+    "noclip",
+};
+static std::set<std::string>
+sVariableNames{
+    "Theatre.draw_3d",
+    "Theatre.draw_2d",
+    "Theatre.debug_save_msgs",
+    "Theatre.draw_text_new",
+};
+static Variables
+sVariables{
+    MakeShared<Variable>("Theatre.draw_3d", true),
+    MakeShared<Variable>("Theatre.draw_2d", true),
+    MakeShared<Variable>("Theatre.debug_save_msgs", false),
+    MakeShared<Variable>("Theatre.draw_text_new", false),
+};
+
+static Error sProcessEngineCommand(Sarg);
+
+void Console::SetCommandCallback(ConsoleCommandCallback_f inCallback)
+{ sCommandCallback = inCallback; }
+
+Error Console::ProcessLine(Sarg inInput)
+{
+    std::string buffer{};
+    std::vector<std::string> shitty_tokens{};
+    for(char character : inInput)
+    {
+        if(sWhitespace.contains(character))
+        {
+            shitty_tokens.push_back(buffer);
+            buffer.clear();
+            continue;
+        }
+        buffer += character;
+    }
+    shitty_tokens.push_back(buffer);
+    for(int i{0}; i < shitty_tokens.size(); ++i)
+    {
+        if(sVariableNames.contains(shitty_tokens[i]))
+        {
+            auto variable{try_GetVariable(shitty_tokens[i])};
+            while(i < shitty_tokens.size() and shitty_tokens[i].compare("=")) { ++i; }
+            if(++i < shitty_tokens.size())
+            {
+                std::string value{shitty_tokens[i]};
+                switch(variable->type)
+                {
+                case Variable::STRING_TYPE:
+                    variable->string_value = value;
+                    break;
+                case Variable::FLOAT_TYPE:
+                    StringToNum(variable->float_value, value);
+                    break;
+                case Variable::INTEGER_TYPE:
+                    StringToNum(variable->int_value, value);
+                    break;
+                }
+            }
+        }
+        else if(sCommands.contains(shitty_tokens[i]))
+        {
+            sProcessEngineCommand(shitty_tokens[i]);
+            if(sCommandCallback) { sCommandCallback(shitty_tokens[i]); }
+            return OK;
+        }
+    }
+    return FAILED;
+}
+
+Shared<Variable> Console::try_GetVariable(Sarg inName)
+{
+    if(sVariables.empty() or !sVariableNames.contains(inName))
+        { return sEmptyVariable; }
+    for(FAUTO var : sVariables)
+        { if(!var->name.compare(inName)) { return var; } }
+    return sEmptyVariable;
+}
+
+Error Console::GetVariable(Sarg inName, Shared<Variable>& outVariable)
+{
+    if(sVariables.empty())
+        { return ERR_EMPTY; }
+    else if(!sVariableNames.contains(inName))
+        { return ERR_NOT_FOUND; }
+    for(FAUTO var : sVariables)
+    {
+        if(!var->name.compare(inName))
+        {
+            outVariable = var;
+            return OK;
+        }
+    }
+    return ERR_NOT_FOUND;
+}
+
+Error Console::SetVariable(Sarg inName, Sarg inValue)
+{
+    if(sVariables.empty())
+        { return ERR_EMPTY; }
+    else if(!sVariableNames.contains(inName))
+        { return ERR_NOT_FOUND; }
+    for(FAUTO var : sVariables)
+        { if(!var->name.compare(inName)) { var->string_value = inValue; } }
+    return OK;
+}
+
+Error Console::SetVariable(Sarg inName, int inValue)
+{
+    if(sVariables.empty())
+        { return ERR_EMPTY; }
+    else if(!sVariableNames.contains(inName))
+        { return ERR_NOT_FOUND; }
+    for(FAUTO var : sVariables)
+        { if(!var->name.compare(inName)) { var->int_value = inValue; } }
+    return OK;
+}
+
+Error Console::SetVariable(Sarg inName, float inValue)
+{
+    if(sVariables.empty())
+        { return ERR_EMPTY; }
+    else if(!sVariableNames.contains(inName))
+        { return ERR_NOT_FOUND; }
+    for(FAUTO var : sVariables)
+        { if(!var->name.compare(inName)) { var->float_value = inValue; } }
+    return OK;
+}
+
+Error Console::AddVariable(Sarg inName, Sarg inInitialValue)
+{
+    if(sVariableNames.contains(inName))
+        { return ERR_ALREADY_EXISTS; }
+    sVariableNames.insert(inName);
+    sVariables.push_back(MakeShared<Variable>(inName, inInitialValue));
+    return OK;
+}
+
+Error Console::AddVariable(Sarg inName, int inInitialValue)
+{
+    if(sVariableNames.contains(inName))
+        { return ERR_ALREADY_EXISTS; }
+    sVariableNames.insert(inName);
+    sVariables.push_back(MakeShared<Variable>(inName, inInitialValue));
+    return OK;
+}
+
+Error Console::AddVariable(Sarg inName, float inInitialValue)
+{
+    if(sVariableNames.contains(inName))
+        { return ERR_ALREADY_EXISTS; }
+    sVariableNames.insert(inName);
+    sVariables.push_back(MakeShared<Variable>(inName, inInitialValue));
+    return OK;
+}
+
+Error Console::RemoveVariable(Sarg inName)
+{
+    if(sVariables.empty())
+        { return ERR_EMPTY; }
+    else if(!sVariableNames.contains(inName))
+        { return ERR_NOT_FOUND; }
+    for(auto iter{sVariables.begin()}; iter != sVariables.end(); ++iter)
+        { if(!iter->get()->name.compare(inName)) { sVariables.erase(iter); } }
+    sVariableNames.erase(inName);
+    return OK;
+}
+
+Farg<Variables> Console::GetVariables()
+{ return sVariables; }
+
+Error Console::AddCommand(Sarg inCommand)
+{
+    return (sCommands.insert(inCommand).second)
+        ? OK
+        : ERR_ALREADY_EXISTS;
+}
+
+Error Console::RemoveCommand(Sarg inCommand)
+{
+    return (sCommands.erase(inCommand))
+        ? OK
+        : (sCommands.empty())
+            ? ERR_EMPTY
+            : ERR_NOT_FOUND;
+}
+
+Farg<Commands> Console::GetCommands()
+{ return sCommands; }
+
+Error sProcessEngineCommand(Sarg inCommand)
+{
+    return OK;
+}
--- Engine/Nostalgia/console/console.hpp
+++ Engine/Nostalgia/console/console.hpp
@@ -3,21 +3,59 @@
 
 #include <Nostalgia/fwd/core.hpp>
 
-typedef Error (*ConsoleCommandCallback_f)(Sarg inCommand);
-
-class DebugConsole
+namespace Console
 {
-public:
-    static Shared<DebugConsole> Instance();
-    static Shared<DebugConsole> Create();
+    struct Variable
+    {
+        enum Type { STRING_TYPE, FLOAT_TYPE, INTEGER_TYPE };
+        Type        type{STRING_TYPE};
+        std::string name{""};
+        std::string string_value{""};
+        int         int_value{0};
+        float       float_value{0.0f};
 
-    virtual void  SetConsoleCommandCallback(ConsoleCommandCallback_f) = 0;
-    virtual Error AddConsoleVariable(Sarg inName);
-    virtual Error RemoveConsoleVariable(Sarg inName);
-    virtual Error SetConsoleVariable(Sarg inName, Sarg inValue);
+        explicit Variable() noexcept = default;
+        explicit Variable(Sarg inName, Sarg inValue) noexcept:
+            type{STRING_TYPE},
+            name{inName},
+            string_value{inValue} {}
+        explicit Variable(Sarg inName, int inValue) noexcept:
+            type{INTEGER_TYPE},
+            name{inName},
+            int_value{inValue} {}
+        explicit Variable(Sarg inName, bool inValue) noexcept:
+            type{INTEGER_TYPE},
+            name{inName},
+            int_value{inValue} {}
+        explicit Variable(Sarg inName, float inValue) noexcept:
+            type{FLOAT_TYPE},
+            name{inName},
+            float_value{inValue} {}
+    };
 
-private:
-    static Shared<DebugConsole> m_sInstance;
+    using Variables = std::vector<Shared<Variable>>;
+    using Commands  = std::set<std::string>;
+
+    typedef void (*ConsoleCommandCallback_f)(Sarg inCommand);
+
+    void SetCommandCallback(ConsoleCommandCallback_f);
+
+    Error ProcessLine(Sarg inInput);
+
+    Shared<Variable> try_GetVariable(Sarg inName);
+    Error GetVariable(Sarg inName, Shared<Variable>& outVariable);
+    Error SetVariable(Sarg inName, Sarg  inValue);
+    Error SetVariable(Sarg inName, int   inValue);
+    Error SetVariable(Sarg inName, float inValue);
+    Error AddVariable(Sarg inName, Sarg  inInitialValue);
+    Error AddVariable(Sarg inName, int   inInitialValue);
+    Error AddVariable(Sarg inName, float inInitialValue);
+    Error RemoveVariable(Sarg inName);
+    Farg<Variables> GetVariables();
+
+    Error AddCommand(Sarg inCommand);
+    Error RemoveCommand(Sarg inCommand);
+    Farg<Commands> GetCommands();
 };
 
 #endif // CONSOLE_H
