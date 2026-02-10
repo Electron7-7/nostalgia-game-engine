a96e085091633aee2f23fbf2d892894635909015
Electron7-7 <26826460+Electron7-7@users.noreply.github.com>
2026-02-10 17:04:51 -0500

    style(formatting)

--- Engine/Nostalgia/filesystem/filesystem.cpp
+++ Engine/Nostalgia/filesystem/filesystem.cpp
@@ -20,24 +20,26 @@ std::string FileSystem::GetCurrentDirectory()
 #   pragma message("FIXME: Improve this (it's from GraphX)")
     std::string FileSystem::GetProgramDirectory()
     {
-        char out_path[MAX_PATH] = {0};
+        char out_path[MAX_PATH]{0};
         GetModuleFileNameA(NULL, out_path, MAX_PATH);
-        std::string buffer = "";
-        bool filename_removed = false;
+        std::string buffer{""};
+        bool filename_removed{false};
         for(int i = (sizeof(out_path)/sizeof(out_path[0])) ; i >= 0 ; --i)
         {
             if(out_path[i] == 0x00)
                 { continue; }
-            if(((out_path[i] == '\\' && !(out_path[i+1] == ' ')) || out_path[i] == '/') && !filename_removed)
+            if(!filename_removed && ((out_path[i] == '\\'
+                    and !(out_path[i+1] == ' '))
+                        or out_path[i] == '/'))
             {
                 buffer = "";
                 filename_removed = true;
             }
             buffer += out_path[i];
         }
-        std::string new_buffer = "";
+        std::string new_buffer{""};
         for(int i = buffer.length() - 1 ; i > 0 ; --i)
-            new_buffer += buffer[i];
+            { new_buffer += buffer[i]; }
         return new_buffer + "\\";
     }
 #else  // !NOSTALGIA_WINDOWS
