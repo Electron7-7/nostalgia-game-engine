c74d72dbdbf2879f7a8a18bd6a41c24db05d35d8
Electron7-7 <26826460+Electron7-7@users.noreply.github.com>
2026-02-27 21:40:41 -0500

    profiling(CreateThingNoReady)

--- Nostalgia/theatre/theatre.cpp
+++ Nostalgia/theatre/theatre.cpp
@@ -515,6 +515,8 @@ ID Theatre::CreateThingNoReady(TheatreFile::ThingData& ioData)
 {
     LockGuard<RMutex> lock{mThingsMutex};
 
+    PRINT_PRETTY_FUNCTION_EXT("BEGIN");
+
     if(ThingFactory::IsDerivedFrom(ioData.type, ThingType::NostalgiaPlayer3D)
         and mThings.contains(UID::a_Player))
             { print_warning("Only one player at a time, please!"); return UID::a_Player; }
@@ -547,6 +549,8 @@ ID Theatre::CreateThingNoReady(TheatreFile::ThingData& ioData)
     }
     else if(ThingFactory::IsDerivedFrom(thing->type(), ThingType::Visual2D))
         { mVisual2DIDs.insert(thing->uid()); }
+
+    PRINT_PRETTY_FUNCTION_EXT("END");
 
     return thing->uid();
 }
