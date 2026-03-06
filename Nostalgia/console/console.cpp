be91ccbaa27667000002a70f72ca0279b2dabe54
Electron7-7 <26826460+Electron7-7@users.noreply.github.com>
2026-03-06 17:34:54 -0500

    feature(Console): added variable for Collider3D to check before printing debug logs

--- Nostalgia/console/console.cpp
+++ Nostalgia/console/console.cpp
@@ -16,6 +16,7 @@ sVariableNames{
     "Theatre.draw_2d",
     "Theatre.debug_save_msgs",
     "Theatre.draw_text_new",
+    "Collider3D.debug_collision_msgs",
 };
 static Variables
 sVariables{
@@ -23,6 +24,7 @@ sVariables{
     MakeShared<Variable>("Theatre.draw_2d", true),
     MakeShared<Variable>("Theatre.debug_save_msgs", false),
     MakeShared<Variable>("Theatre.draw_text_new", false),
+    MakeShared<Variable>("Collider3D.debug_collision_msgs", false),
 };
 
 static Error sProcessEngineCommand(Sarg);
