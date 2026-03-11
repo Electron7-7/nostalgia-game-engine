5438f50a941774d2970258a688fd676c13399d0e
Electron7-7 <26826460+Electron7-7@users.noreply.github.com>
2026-03-10 22:42:57 -0400

    feature(ImageHandler): implemented an image-handling namespace which implements [STBI](https://github.com/nothings/stb) I may move this around, but the main idea is to keep STB-related code in this namespace.

--- Sources.cmake
+++ Sources.cmake
@@ -89,6 +89,7 @@ set(NOSTALGIA_SRC_FILES
     ${NOSTALGIA_ROOT}/theatre/tree.cpp
     ${NOSTALGIA_ROOT}/filesystem/file_data.cpp
     ${NOSTALGIA_ROOT}/filesystem/filesystem.cpp
+    ${NOSTALGIA_ROOT}/filesystem/image_handler.cpp
     ${NOSTALGIA_ROOT}/physics/engine.cpp
     ${NOSTALGIA_ROOT}/thirdparty/glm/detail/glm.cpp
     ${NOSTALGIA_ROOT}/thirdparty/glad/glad.c
@@ -175,6 +176,7 @@ set(NOSTALGIA_HEADERS
     ${NOSTALGIA_ROOT}/theatre/thing_factory.hpp
     ${NOSTALGIA_ROOT}/filesystem/file_data.hpp
     ${NOSTALGIA_ROOT}/filesystem/filesystem.hpp
+    ${NOSTALGIA_ROOT}/filesystem/image_handler.hpp
     ${NOSTALGIA_ROOT}/physics/engine.hpp
     ${NOSTALGIA_ROOT}/rendering/graphics_context.hpp
     ${NOSTALGIA_ROOT}/rendering/renderer_api.hpp
