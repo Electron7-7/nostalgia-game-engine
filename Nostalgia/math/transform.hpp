04f3143e704e14bbfe8d12af44c63fa89b1c7463
Electron7-7 <26826460+Electron7-7@users.noreply.github.com>
2026-03-26 17:13:05 -0400

    feature(Transform3D): implemented matrix getter functions for the translation, scaling, rotation, and model matrices

--- Nostalgia/math/transform.hpp
+++ Nostalgia/math/transform.hpp
@@ -16,6 +16,18 @@ struct Transform3D
         euler_rotation_degrees{0.0f},
         scale{1.0f};
     glm::quat quaternion{euler_rotation_radians};
+
+    glm::mat4 translation_matrix() const
+    { return glm::translate(glm::mat4{1.0f}, position); }
+
+    glm::mat4 scaling_matrix() const
+    { return glm::scale(glm::mat4{1.0f}, scale); }
+
+    glm::mat4 rotation_matrix() const
+    { return glm::mat4_cast(glm::normalize(quaternion)); }
+
+    glm::mat4 model_matrix() const
+    { return translation_matrix() * rotation_matrix() * scaling_matrix(); }
 };
 
 #endif // TRANSFORM_H
