49efd15546be36c3d59626e301bf7c80bc556cf5
Electron7-7 <26826460+Electron7-7@users.noreply.github.com>
2026-03-30 21:10:36 -0400

    feature(VariableRegistry): add more enums

--- Nostalgia/theatre/variable_registry.cpp
+++ Nostalgia/theatre/variable_registry.cpp
@@ -13,16 +13,24 @@ Farg<VariableRegistry::Enums> VariableRegistry::GetRegisteredEnums()
 
 void VariableRegistry::RegisterEngineEnums()
 {
-    m_sEnums["Static"]         = MotionType::Static;
-    m_sEnums["Dynamic"]        = MotionType::Dynamic;
-    m_sEnums["Kinematic"]      = MotionType::Kinematic;
-    m_sEnums["Box"]            = ShapeType::Box;
-    m_sEnums["Capsule"]        = ShapeType::Capsule;
-    m_sEnums["Cylinder"]       = ShapeType::Cylinder;
-    m_sEnums["Sphere"]         = ShapeType::Sphere;
-    m_sEnums["2DTexture"]      = TextureType::TEXTURE_TYPE_2D;
-    m_sEnums["CubeMapTexture"] = TextureType::TEXTURE_TYPE_CUBE;
-    m_sEnums["CustomColor"]    = Environment::BG_CUSTOM_COLOR;
-    m_sEnums["ClearColor"]     = Environment::BG_CLEAR_COLOR;
-    m_sEnums["Skybox"]         = Environment::BG_SKYBOX;
+    m_sEnums["Static"]                 = MotionType::Static;
+    m_sEnums["Dynamic"]                = MotionType::Dynamic;
+    m_sEnums["Kinematic"]              = MotionType::Kinematic;
+    m_sEnums["Box"]                    = ShapeType::Box;
+    m_sEnums["Capsule"]                = ShapeType::Capsule;
+    m_sEnums["Cylinder"]               = ShapeType::Cylinder;
+    m_sEnums["Sphere"]                 = ShapeType::Sphere;
+    m_sEnums["2DTexture"]              = TextureType::TEXTURE_TYPE_2D;
+    m_sEnums["CubeMapTexture"]         = TextureType::TEXTURE_TYPE_CUBE;
+    m_sEnums["CustomColor"]            = Environment::BG_CUSTOM_COLOR;
+    m_sEnums["ClearColor"]             = Environment::BG_CLEAR_COLOR;
+    m_sEnums["Skybox"]                 = Environment::BG_SKYBOX;
+    m_sEnums["SamplerNoFilter"]        = SAMPLER_FILTER_NONE;
+    m_sEnums["SamplerFilterLinear"]    = SAMPLER_FILTER_LINEAR;
+    m_sEnums["SamplerFilterNearest"]   = SAMPLER_FILTER_NEAREST;
+    m_sEnums["SamplerRepeat"]          = SAMPLER_REPEAT_MODE_REPEAT;
+    m_sEnums["SamplerRepeatMirrored"]  = SAMPLER_REPEAT_MODE_MIRRORED_REPEAT;
+    m_sEnums["SamplerClampEdge"]       = SAMPLER_REPEAT_MODE_CLAMP_TO_EDGE;
+    m_sEnums["SamplerClampBorder"]     = SAMPLER_REPEAT_MODE_CLAMP_TO_BORDER;
+    m_sEnums["SamplerMirrorClampEdge"] = SAMPLER_REPEAT_MODE_MIRROR_CLAMP_TO_EDGE;
 }
