4156b8547ee87bd2e36008b96403ad335ec0567f
Electron7-7 <26826460+Electron7-7@users.noreply.github.com>
2026-04-16 22:10:04 -0400

    version number

--- Nostalgia/Nostalgia.hpp
+++ Nostalgia/Nostalgia.hpp
@@ -5,7 +5,7 @@
 #define __n_make_string(x) __n_make_string_2(x)
 
 #define NOSTALGIA_VERSION_MAJOR 0
-#define NOSTALGIA_VERSION_MINOR 15
+#define NOSTALGIA_VERSION_MINOR 16
 #define NOSTALGIA_VERSION_PATCH 0
 #define NOSTALGIA_VERSION_STRING \
     __n_make_string(NOSTALGIA_VERSION_MAJOR) "." \
