a6ba59704ce6ec642ececf2e25fb60a64f136f65
Electron7-7 <26826460+Electron7-7@users.noreply.github.com>
2026-04-22 17:27:49 -0400

    feature(OpenGLTextureBuffer): implement changes to `TextureBuffer` and the new `Convert` namespace

--- Nostalgia/backends/opengl/gl_texture_buffer.cpp
+++ Nostalgia/backends/opengl/gl_texture_buffer.cpp
@@ -1,153 +1,87 @@
-#include "gl_texture_buffer.hpp"
+#include "./gl_texture_buffer.hpp"
+#include "./gl_enum_conversions.hpp"
 #include "managers/render_manager.hpp"
 #include "thirdparty/glad/glad.h"
 
-static GLenum s_GLType(TextureType inType)
+#define ASSERT_API if(not g_pRenderManager->IsAPIActive()) { return; }
+
+OpenGLTextureBuffer::OpenGLTextureBuffer()
 {
-    switch(inType)
-    {
-    case TEXTURE_TYPE_1D:
-        return GL_TEXTURE_1D;
-    case TEXTURE_TYPE_2D:
-        return GL_TEXTURE_2D;
-    case TEXTURE_TYPE_3D:
-        return GL_TEXTURE_3D;
-    case TEXTURE_TYPE_1D_ARRAY:
-        return GL_TEXTURE_1D_ARRAY;
-    case TEXTURE_TYPE_2D_ARRAY:
-        return GL_TEXTURE_2D_ARRAY;
-    case TEXTURE_TYPE_CUBE_ARRAY:
-        return GL_TEXTURE_CUBE_MAP_ARRAY;
-    case TEXTURE_TYPE_CUBE:
-        return GL_TEXTURE_CUBE_MAP;
-    case TEXTURE_TYPE_NONE:
-    default:
-        return GL_TEXTURE_2D;
-    }
+    ASSERT_API
+    glGenTextures(1, &mBufferID);
 }
 
-static GLint s_GLFormat(DataFormat inFormat, bool getInternal = false)
+OpenGLTextureBuffer::~OpenGLTextureBuffer()
 {
-    switch(inFormat)
-    {
-    case DATA_FORMAT_RED:
-        return GL_RED;
-    case DATA_FORMAT_RGB:
-        if(getInternal) { return GL_RGB8; }
-        else            { return GL_RGB;  }
-    case DATA_FORMAT_SRGB:
-        if(getInternal) { return GL_SRGB8; }
-        else            { return GL_RGB;   }
-    case DATA_FORMAT_RGBA:
-    case DATA_FORMAT_RGBA8:
-        if(getInternal) { return GL_RGBA8; }
-        else            { return GL_RGBA;  }
-    case DATA_FORMAT_SRGB_ALPHA:
-        if(getInternal) { return GL_SRGB8_ALPHA8; }
-        else            { return GL_RGBA;         }
-    case DATA_FORMAT_NONE:
-    default:
-        print_error("Invalid data format! Returning `GL_NONE` (this could cause a crash!)");
-        return GL_NONE;
-    }
+    ASSERT_API
+    glDeleteTextures(1, &mBufferID);
 }
 
-static GLenum s_GLFilter(SamplerFilter inFilter, SamplerFilter inMipFilter)
+void OpenGLTextureBuffer::GenerateMipMaps()
 {
-    if(inFilter == SAMPLER_FILTER_NONE)
-        { return GL_LINEAR; } // idk if `GL_NONE` is valid so just return `GL_LINEAR`
-    uint has_mip = (inMipFilter != SAMPLER_FILTER_NONE)
-        ? 0b100000000
-        : 0b0;
-    uint out{GL_NEAREST | has_mip};
-    if(inMipFilter == SAMPLER_FILTER_LINEAR)
-        { out += 2; }
-    if(inFilter == SAMPLER_FILTER_LINEAR)
-        { ++out; }
-    return out;
-}
-
-static GLenum s_GLWrap(SamplerRepeat inRepeat)
-{
-    switch(inRepeat)
-    {
-        case SAMPLER_REPEAT_MODE_REPEAT:
-            return GL_REPEAT;
-        case SAMPLER_REPEAT_MODE_MIRRORED_REPEAT:
-            return GL_MIRRORED_REPEAT;
-        case SAMPLER_REPEAT_MODE_CLAMP_TO_EDGE:
-            return GL_CLAMP_TO_EDGE;
-        case SAMPLER_REPEAT_MODE_CLAMP_TO_BORDER:
-            return GL_CLAMP_TO_BORDER;
-        case SAMPLER_REPEAT_MODE_MIRROR_CLAMP_TO_EDGE:
-            return GL_MIRROR_CLAMP_TO_EDGE;
-    }
-}
-
-Error OpenGLTextureBuffer::GenerateMipMaps()
-{
-    glBindTexture(s_GLType(mType), mBufferID);
+    ASSERT_API
+    glBindTexture(Convert::GL_TextureType(mType), mBufferID);
     glGenerateTextureMipmap(mBufferID);
-    glBindTexture(s_GLType(mType), 0);
-    return OK;
+    glBindTexture(Convert::GL_TextureType(mType), 0);
 }
 
-Error OpenGLTextureBuffer::SetSamplerState(Farg<SamplerState> inSamplerState) const
+void OpenGLTextureBuffer::SetSamplerState(Farg<SamplerState> inSamplerState) const
 {
-    glBindTexture(s_GLType(mType), mBufferID);
+    ASSERT_API
+    glBindTexture(Convert::GL_TextureType(mType), mBufferID);
     if(inSamplerState.use_anisotropy)
         { glTextureParameterf(mBufferID, GL_TEXTURE_MAX_ANISOTROPY, inSamplerState.anisotropy_max); }
-    glTextureParameteri(mBufferID, GL_TEXTURE_MIN_FILTER, s_GLFilter(inSamplerState.min_filter, inSamplerState.mip_filter_min));
-    glTextureParameteri(mBufferID, GL_TEXTURE_MAG_FILTER, s_GLFilter(inSamplerState.mag_filter, SAMPLER_FILTER_NONE));
-    glTextureParameteri(mBufferID, GL_TEXTURE_WRAP_S, s_GLWrap(inSamplerState.repeat_u));
-    glTextureParameteri(mBufferID, GL_TEXTURE_WRAP_T, s_GLWrap(inSamplerState.repeat_v));
-    glTextureParameteri(mBufferID, GL_TEXTURE_WRAP_R, s_GLWrap(inSamplerState.repeat_w));
-    glBindTexture(s_GLType(mType), 0);
-
-    if(mType == TEXTURE_TYPE_NONE)
-        { return FAILED; }
-    else if(!mStatus)
-        { return mStatus; }
-    return OK;
+    glTextureParameteri(mBufferID, GL_TEXTURE_MIN_FILTER, Convert::GL_SamplerFilter(inSamplerState.min_filter, inSamplerState.mip_filter_min));
+    glTextureParameteri(mBufferID, GL_TEXTURE_MAG_FILTER, Convert::GL_SamplerFilter(inSamplerState.mag_filter, SAMPLER_FILTER_NONE));
+    glTextureParameteri(mBufferID, GL_TEXTURE_WRAP_S, Convert::GL_SamplerRepeat(inSamplerState.repeat_u));
+    glTextureParameteri(mBufferID, GL_TEXTURE_WRAP_T, Convert::GL_SamplerRepeat(inSamplerState.repeat_v));
+    glTextureParameteri(mBufferID, GL_TEXTURE_WRAP_R, Convert::GL_SamplerRepeat(inSamplerState.repeat_w));
+    glBindTexture(Convert::GL_TextureType(mType), 0);
 }
 
-Error OpenGLTextureBuffer::Load(InputData inData, Farg<TextureFormat> inFormat, int inLayer)
+void OpenGLTextureBuffer::Load(InputData inData, Farg<TextureFormat> inFormat, int inLayer)
 {
-    GLenum gl_type{s_GLType(mType = inFormat.type)};
+    ASSERT_API
+    GLenum gl_type{Convert::GL_TextureType(mType = inFormat.type)};
     GLenum gl_target{(gl_type == GL_TEXTURE_CUBE_MAP)
         ? GL_TEXTURE_CUBE_MAP_POSITIVE_X + inLayer
         : gl_type};
 
     glBindTexture(gl_type, mBufferID);
-    if(inFormat.data_format == DATA_FORMAT_RED) { glPixelStorei(GL_UNPACK_ALIGNMENT, 1); }
+    if(inFormat.data_format == DATA_FORMAT_RED)
+        { glPixelStorei(GL_UNPACK_ALIGNMENT, 1); }
     glTexImage2D(gl_target,
         0,
-        s_GLFormat(inFormat.data_format, true),
+        Convert::GL_DataFormat(inFormat.data_format, true),
         inFormat.width,
         inFormat.height,
         0,
-        s_GLFormat(inFormat.data_format),
+        Convert::GL_DataFormat(inFormat.data_format),
         GL_UNSIGNED_BYTE,
         inData);
-
     glBindTexture(gl_type, 0);
-    return mStatus = OK;
 }
 
-OpenGLTextureBuffer::OpenGLTextureBuffer()
-{ glGenTextures(1, &mBufferID); }
-
-OpenGLTextureBuffer::~OpenGLTextureBuffer()
-{
-    if(g_pRenderManager->GetAPI()->IsRunning())
-        { glDeleteTextures(1, &mBufferID); }
-}
-
-Error OpenGLTextureBuffer::Status() const
-{ return mStatus; }
+TextureType OpenGLTextureBuffer::Type() const
+{ return mType; }
 
 uint OpenGLTextureBuffer::ID() const
 { return mBufferID; }
 
-TextureType OpenGLTextureBuffer::Type() const
-{ return mType; }
+Shared<Image> OpenGLTextureBuffer::GetImage(int inMipmapLevel) const
+{
+    if(not g_pRenderManager->IsAPIActive())
+        { return Image::CreateEmpty(1, 1); }
+    GLint _width, _height;
+    glGetTextureLevelParameteriv(mBufferID, inMipmapLevel, GL_TEXTURE_WIDTH, &_width);
+    glGetTextureLevelParameteriv(mBufferID, inMipmapLevel, GL_TEXTURE_HEIGHT, &_height);
+    std::vector<uchar> _data(4 * _width * _height);
+    glPixelStorei(GL_UNPACK_ALIGNMENT, 4);
+    glGetTextureImage(mBufferID,
+        inMipmapLevel,
+        GL_RGBA,
+        GL_UNSIGNED_BYTE,
+        _data.size(),
+        _data.data());
+    return Image::CreateFromData(_data.data(), _data.size(), _width, _height, 4, false);
+}
--- Nostalgia/backends/opengl/gl_texture_buffer.hpp
+++ Nostalgia/backends/opengl/gl_texture_buffer.hpp
@@ -1,7 +1,7 @@
 #ifndef GL_TEXTURE_BUFFER_H
 #define GL_TEXTURE_BUFFER_H
 
-#include <Nostalgia/rendering/texture_buffer.hpp>
+#include "rendering/texture_buffer.hpp"
 
 class OpenGLTextureBuffer final : public TextureBuffer
 {
@@ -9,17 +9,16 @@ public:
     OpenGLTextureBuffer();
     ~OpenGLTextureBuffer();
 
-    Error GenerateMipMaps() final;
-    Error SetSamplerState(Farg<SamplerState>) const final;
-    Error Load(InputData, Farg<TextureFormat>, int) final;
+    void GenerateMipMaps() final;
+    void SetSamplerState(Farg<SamplerState>) const final;
+    void Load(InputData, Farg<TextureFormat>, int) final;
     TextureType Type() const final;
-    Error Status() const final;
     uint ID() const final;
+    Shared<Image> GetImage(int = 0) const final;
 
 private:
     uint mBufferID{0};
     TextureType mType{TEXTURE_TYPE_NONE};
-    Error mStatus{ERR_UNINITIALIZED};
 };
 
 #endif // GL_TEXTURE_BUFFER_H
