5dc20aa5044abd95d5c64632e5552f9f3b3322ab
Electron7-7 <26826460+Electron7-7@users.noreply.github.com>
2026-04-27 14:53:00 -0400

    feature(VertexArray): implement `DummyVertexArray`

--- Nostalgia/rendering/vertex_array.cpp
+++ Nostalgia/rendering/vertex_array.cpp
@@ -3,6 +3,9 @@
 // Implementations
 #include "backends/opengl/gl_vertex_array.hpp"
 
+Shared<VertexArray> VertexArray::CreateDummy()
+{ return MakeShared<DummyVertexArray>(); }
+
 Shared<VertexArray> VertexArray::Create()
 {
     switch(RendererAPI::CurrentAPI())
@@ -10,7 +13,7 @@ Shared<VertexArray> VertexArray::Create()
     case RendererAPI::NONE:
     default:
         print_warning(RendererAPI::s_cAPIWarningMessage);
-        [[fallthrough]];
+        return CreateDummy();
     case RendererAPI::OPENGL:
         return MakeShared<OpenGLVertexArray>();
     }
--- Nostalgia/rendering/vertex_array.hpp
+++ Nostalgia/rendering/vertex_array.hpp
@@ -18,11 +18,27 @@ public:
     virtual void SetIndexBuffer(Shared<IndexBuffer> inIndexBuffer) = 0;
     virtual Shared<IndexBuffer> GetIndexBuffer() const = 0;
 
+    static Shared<VertexArray> CreateDummy();
     static Shared<VertexArray> Create();
 
 protected:
     std::vector<Shared<VertexBuffer>> mVertexBuffers{};
     Shared<IndexBuffer> mIndexBuffer{nullptr};
+};
+
+class DummyVertexArray final : public VertexArray
+{
+public:
+    DummyVertexArray() = default;
+    virtual ~DummyVertexArray() = default;
+
+    void Bind()   const final {}
+    void Unbind() const final {}
+    uint GetID()  const final { return 0; }
+
+    void AddVertexBuffer(Shared<VertexBuffer>) final {}
+    void SetIndexBuffer(Shared<IndexBuffer>) final {}
+    Shared<IndexBuffer> GetIndexBuffer() const final { return IndexBuffer::CreateDummy(); }
 };
 
 #endif // VERTEX_ARRAY_H
