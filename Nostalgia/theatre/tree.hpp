42ea9bd99681bc331fb5d7a579a51e097437fbe2
Electron7-7 <26826460+Electron7-7@users.noreply.github.com>
2026-05-01 18:53:49 -0400

    feature(CallSheet,Tree): remove `CallSheet` and move its functionality to `Tree`

--- Nostalgia/theatre/tree.hpp
+++ Nostalgia/theatre/tree.hpp
@@ -1,8 +1,6 @@
 #ifndef TREE_H
 #define TREE_H
 
-struct Tree; // Forward Declaration
-
 struct Node
 {
     bool invalid() const noexcept;
@@ -14,11 +12,18 @@ struct Node
 
 struct Tree
 {
-    std::unordered_map<ID, Node> all_nodes{};
+    std::unordered_map<ID, Node> nodes{};
 
     Error add_node(ID inNodeID, ID inParentID = ID::Invalid) noexcept;
     Error remove_node(ID inNodeID) noexcept;
     Error set_parent(ID inNodeID, ID inParentID) noexcept;
+    IdSet_t get_descendants(ID inNodeID) noexcept;
+    IdSet_t get_ancestors(ID inNodeID) noexcept;
+    Farg<Node> get_node(ID inNodeID) const noexcept;
+    bool has_node(ID inNodeID) const noexcept;
+    void clear() noexcept;
+
+    std::string debug_log() const noexcept;
 };
 
 #endif // TREE_H
