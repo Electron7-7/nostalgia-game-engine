6f4a1b9579ec3f3aff93899c2fb08d4560390270
Electron7-7 <26826460+Electron7-7@users.noreply.github.com>
2026-05-01 19:29:51 -0400

    feature(Tree): move `Tree` code to the header and make `Tree` and `Node` both templates that take either `ID` or `PID`

--- Nostalgia/core/tree.hpp
+++ Nostalgia/core/tree.hpp
@@ -1,29 +1,139 @@
 #ifndef TREE_H
 #define TREE_H
 
-struct Node
-{
-    bool invalid() const noexcept;
+template<typename T> requires std::same_as<T, ID> or std::same_as<T, PID>
+    struct Node
+    {
+        bool invalid() const noexcept
+        { return id.invalid(); }
 
-    ID id{};
-    ID parent{};
-    IdSet_t children{};
-};
+        T id{};
+        T parent{};
+        std::set<T> children{};
+    };
 
-struct Tree
-{
-    std::unordered_map<ID, Node> nodes{};
+template<typename T> requires std::same_as<T, ID> or std::same_as<T, PID>
+    struct Tree
+    {
+        std::unordered_map<T, Node<T>> nodes{};
 
-    Error add_node(ID inNodeID, ID inParentID = ID::Invalid) noexcept;
-    Error remove_node(ID inNodeID) noexcept;
-    Error set_parent(ID inNodeID, ID inParentID) noexcept;
-    IdSet_t get_descendants(ID inNodeID) noexcept;
-    IdSet_t get_ancestors(ID inNodeID) noexcept;
-    Farg<Node> get_node(ID inNodeID) const noexcept;
-    bool has_node(ID inNodeID) const noexcept;
-    void clear() noexcept;
+        Error add_node(Farg<T> inNodeID, Farg<T> inParentID = T{}) noexcept
+        {
+            if(nodes.contains(inNodeID))
+                { return ERR_ALREADY_EXISTS; }
+            else if(!inParentID.invalid())
+            {
+                if(!nodes.contains(inParentID))
+                    { return ERR_NOT_FOUND; }
+                nodes.at(inParentID).children.insert(inNodeID);
+            }
+            nodes.emplace(inNodeID, Node{inNodeID, inParentID});
+            return OK;
+        }
 
-    std::string debug_log() const noexcept;
-};
+        Error remove_node(Farg<T> inNodeID) noexcept
+        {
+            if(auto found_it{nodes.find(inNodeID)}; found_it != nodes.end())
+            {
+                if(auto found_parent{nodes.find(found_it->second.parent)};
+                    found_parent != nodes.end())
+                        { found_parent->second.children.erase(inNodeID); }
+                for(FAUTO child : found_it->second.children)
+                {
+                    if(auto found_child{nodes.find(child)}; found_child != nodes.end())
+                        { found_child->second.parent = T{}; }
+                }
+                nodes.erase(found_it);
+                return OK;
+            }
+            return ERR_NOT_FOUND;
+        }
+
+        Error set_parent(Farg<T> inNodeID, Farg<T> inParentID) noexcept
+        {
+            if(!nodes.contains(inNodeID)
+                or (!inParentID.invalid() and !nodes.contains(inParentID)))
+                    { return ERR_NOT_FOUND; }
+
+            auto& child{nodes.at(inNodeID)};
+            if(auto found_it{nodes.find(child.parent)}; found_it != nodes.end())
+                { found_it->second.children.erase(inNodeID); }
+            if(!inParentID.invalid())
+                { nodes.at(inParentID).children.insert(inNodeID); }
+            child.parent = inParentID;
+            return OK;
+        }
+
+        std::set<T> get_descendants(Farg<T> inNodeID) const noexcept
+        {
+// TODO: I should implement an actual root node instead of doing this
+            if(inNodeID.invalid())
+            {
+                auto keys{std::views::keys(nodes)};
+                return {keys.begin(), keys.end()};
+            }
+            else if(!nodes.contains(inNodeID))
+                { return {}; }
+            std::set<T> output{};
+            FAUTO children{nodes.at(inNodeID).children};
+            for(FAUTO child : children)
+            {
+                output.insert(child);
+                auto sub_children{get_descendants(child)};
+                output.insert(sub_children.cbegin(), sub_children.cend());
+            }
+            return output;
+        }
+
+        std::set<T> get_ancestors(Farg<T> inNodeID) const noexcept
+        {
+            if(!nodes.contains(inNodeID))
+                { return {}; }
+            std::set<T> output{};
+            T parent{nodes.at(inNodeID).parent};
+            while(not parent.invalid())
+            {
+                if(nodes.contains(parent))
+                {
+                    output.insert(parent);
+                    parent = nodes.at(parent).parent;
+                }
+                else
+                    { parent = {}; }
+            }
+            return output;
+        }
+
+        std::vector<T> get_all_nodes() const noexcept
+        {
+            auto keys{std::views::keys(nodes)};
+            return {keys.begin(), keys.end()};
+        }
+
+        Farg<Node<T>> get_node(Farg<T> inNodeID) const noexcept
+        {
+            static Node<T> error_node{};
+// TODO: I should implement an actual root node instead of doing this
+            static Node<T> root_node{};
+            if(inNodeID.invalid())
+            {
+                for(FAUTO [id, node] : nodes)
+                {
+                    if(node.parent.invalid())
+                        { root_node.children.insert(id); }
+                }
+                return root_node;
+            }
+            else if(auto found_it{nodes.find(inNodeID)}; found_it != nodes.end())
+                { return found_it->second; }
+            return error_node;
+        }
+
+        bool has_node(Farg<T> inNodeID) const noexcept
+        { return nodes.contains(inNodeID); }
+
+        void clear() noexcept
+        { nodes.clear(); }
+    };
 
 #endif // TREE_H
