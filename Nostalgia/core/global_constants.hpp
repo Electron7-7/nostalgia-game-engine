fbc65de46702c05da90ce6a170951b186893a271
Electron7-7 <26826460+Electron7-7@users.noreply.github.com>
2026-05-05 16:25:15 -0400

    feature(GlobalConstants): variant type names

--- Nostalgia/core/global_constants.hpp
+++ Nostalgia/core/global_constants.hpp
@@ -11,6 +11,20 @@ namespace GlobalConstants
             cstr_Invalid[]{"Invalid"},
             cstr_true[]{"true"},
             cstr_false[]{"false"};
+
+        namespace VariantType
+        {
+            constinit const char cstr_Nil[]{"Nil"},
+                cstr_String[]{"String"},
+                cstr_Bool[]{"Bool"},
+                cstr_Int[]{"Int"},
+                cstr_Float[]{"Float"},
+                cstr_Vector2[]{"Vector2"},
+                cstr_Vector3[]{"Vector3"},
+                cstr_Vector4[]{"Vector4"},
+                cstr_Quaternion[]{"Quaternion"},
+                cstr_Thing[]{"Thing"};
+        }
     }
 
     const std::string str_empty{Init::cstr_empty},
@@ -18,6 +32,20 @@ namespace GlobalConstants
         str_Invalid{Init::cstr_Invalid},
         str_true{Init::cstr_true},
         str_false{Init::cstr_false};
+
+    namespace VariantType
+    {
+    const std::string str_Nil{Init::VariantType::cstr_Nil},
+        str_String{Init::VariantType::cstr_String},
+        str_Bool{Init::VariantType::cstr_Bool},
+        str_Int{Init::VariantType::cstr_Int},
+        str_Float{Init::VariantType::cstr_Float},
+        str_Vector2{Init::VariantType::cstr_Vector2},
+        str_Vector3{Init::VariantType::cstr_Vector3},
+        str_Vector4{Init::VariantType::cstr_Vector4},
+        str_Quaternion{Init::VariantType::cstr_Quaternion},
+        str_Thing{Init::VariantType::cstr_Thing};
+    }
 }
 
 #endif // GLOBAL_CONSTANTS_H
