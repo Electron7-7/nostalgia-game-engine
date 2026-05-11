2b7a7328304b1b8c70634df52975f753c3b68752
Electron7-7 <26826460+Electron7-7@users.noreply.github.com>
2026-05-11 17:17:45 -0400

    feature(EditorTheatre): implement new player helper model and optimize helper model rendering

--- Editor/theatre/editor_theatre.cpp
+++ Editor/theatre/editor_theatre.cpp
@@ -25,9 +25,27 @@ bool EditorTheatre::Startup()
     m_spSpotLight3DMesh  =
         ArrayMesh::CreateFromMemory(_EditorModels::SpotLight,
             std::size(_EditorModels::SpotLight), ArrayMesh::MODEL_OBJ);
-    m_spPlayerAxis3DMesh =
+    m_sp3DAxisMesh =
         ArrayMesh::CreateFromMemory(_EditorModels::DebugAxis,
             std::size(_EditorModels::DebugAxis), ArrayMesh::MODEL_OBJ);
+    m_spPlayer3DMesh =
+        ArrayMesh::CreateFromMemory(_EditorModels::Player3D,
+            std::size(_EditorModels::Player3D), ArrayMesh::MODEL_OBJ);
+
+    TheatreFile::ThingData cam_dat{ThingType::Camera3D, "EditorCamera3D"};
+    cam_dat.set_variable(glm::vec3{1.2f, 6.3f, 5.6f}, "Position");
+    cam_dat.set_variable(glm::vec3{-26.8f, 7.7f, 0.0f}, "RotationDegrees");
+    cam_dat.set_variable(true, "UseDefaultSkybox");
+    cam_dat.set_variable(true, "Current");
+    m_pEditorCamera3D->SetVariables(cam_dat);
+    m_pEditorCamera3D->Ready();
+    m_pEditorCamera2D->Ready();
+    m_pEditor3DViewport->Ready();
+    m_pEditor2DViewport->Ready();
+    m_pEditor3DViewport->SetCurrentCamera3D(m_pEditorCamera3D);
+    m_pEditor2DViewport->SetCurrentCamera2D(m_pEditorCamera2D);
+    mViewports.insert(m_pEditor3DViewport);
+    mViewports.insert(m_pEditor2DViewport);
 
     return Theatre::Startup();
 }
@@ -37,162 +55,91 @@ bool EditorTheatre::Shutdown()
     m_spCamera3DMesh.reset();
     m_spPointLight3DMesh.reset();
     m_spSpotLight3DMesh.reset();
-    m_spPlayerAxis3DMesh.reset();
+    m_sp3DAxisMesh.reset();
+    m_spPlayer3DMesh.reset();
     return Theatre::Shutdown();
 }
 
 void EditorTheatre::Draw()
 {
     bool _enable_3d_rendering = Console::GetVariable("Theatre.draw_3d").int_value,
-        _enable_2d_rendering = Console::GetVariable("Theatre.draw_2d").int_value;
+        _enable_2d_rendering = Console::GetVariable("Theatre.draw_2d").int_value,
+        _enable_3d_helpers = not Console::GetVariable("Editor.nodraw_3d_helpers").int_value;
 
     LockGuard<RMutex> _things_lock{mThingsMutex};
+    auto* _renderer_api{RendererAPI::Get()};
+    auto _fullbright_shader{_renderer_api->GetShader(Shaders::Fullbright)};
+    auto _helper_mesh{m_sp3DAxisMesh};
 
     Light3D::ClearCounts();
     for(ID id : mLightIDs)
-        { RendererAPI::Get()->SetLight_TempBlinnPhongSolution(GetThinker<Light3D>(id)); }
+        { _renderer_api->SetLight_TempBlinnPhongSolution(GetThinker<Light3D>(id)); }
 
-    for(ID viewport_id : mViewportIDs)
+    for(auto& viewport : mViewports)
     {
-        auto viewport{GetThinker<Viewport>(viewport_id)};
-
         viewport->Attach();
 
-        auto _camera3d{GetThinker<Camera3D>(viewport->CurrentCamera3D())};
-        auto _camera2d{GetThinker<Camera2D>(viewport->CurrentCamera2D())};
-
-        if(_enable_3d_rendering and not _camera3d->invalid())
+        if(_enable_3d_rendering and not viewport->CurrentCamera3D()->invalid())
         {
-            _camera3d->DrawBackground();
+            viewport->CurrentCamera3D()->DrawBackground();
             for(ID _uid : mVisual3DIDs)
-                { _camera3d->Draw(GetThinker<Visual3D>(_uid)); }
+                { viewport->CurrentCamera3D()->Draw(GetThinker<Visual3D>(_uid)); }
         }
-        if(_enable_2d_rendering and not _camera2d->invalid())
+        if(_enable_2d_rendering and not viewport->CurrentCamera2D()->invalid())
         {
             for(ID _uid : mVisual2DIDs)
-                { _camera2d->Draw(GetThinker<Visual2D>(_uid)); }
+                { viewport->CurrentCamera2D()->Draw(GetThinker<Visual2D>(_uid)); }
+        }
+        if(not _enable_3d_helpers)
+        {
+            viewport->Detach();
+            continue;
         }
 
         bool _wireframe{RendererAPI::Get()->GetWireframe()};
-
         RendererAPI::Get()->SetWireframe(false);
+        _fullbright_shader->Bind();
+        _fullbright_shader->SetUniform("current_material.use_diffuse", 0);
+        _fullbright_shader->SetUniform("current_material.use_specular", 0);
+        _fullbright_shader->SetUniform("current_material.alpha", 1.0f);
+        _fullbright_shader->SetUniform("projection_matrix", viewport->CurrentCamera3D()->ProjectionMatrix());
+        _fullbright_shader->SetUniform("debug_output",Console::GetVariable("BlinnPhong.debug_visual").int_value);
+        _fullbright_shader->SetUniform("view_matrix", viewport->CurrentCamera3D()->ViewMatrix());
+        _fullbright_shader->SetUniform("view_position", viewport->CurrentCamera3D()->GlobalPosition());
 
         for(auto& [uid, thing] : mThings)
         {
-            if(uid == _camera3d->uid() or uid == _camera2d->uid())
+            auto _thing3d{DCast<Actor3D>(thing)};
+            if(uid == viewport->CurrentCamera3D()->uid()
+                or not _thing3d
+                or not _enable_3d_rendering
+                or viewport->CurrentCamera3D()->invalid())
                 { continue; }
-            FPID _type{thing->Type()};
-            if(_enable_2d_rendering and not _camera2d->invalid())
+            _fullbright_shader->SetUniform("model_matrix", _thing3d->GlobalTransform().model_matrix());
+            _fullbright_shader->SetUniform("debug_highlight", _thing3d->DebugHighlight());
+            if(thing->DerivedFrom(ThingType::Camera3D))
+                { _helper_mesh = m_spCamera3DMesh; }
+            else if(thing->DerivedFrom(EditorPlayer3D::sClassType()))
+                { _helper_mesh = m_spPlayer3DMesh; }
+            else if(thing->DerivedFrom(ThingType::Light3D)
+                and not thing->DerivedFrom(ThingType::DirectionalLight3D))
             {
-                if(ThingFactory::IsDerivedFrom(_type, ThingType::Camera2D))
-                    { DrawCamera2DHelper(_camera2d, DCast<Camera2D>(thing)); }
+                _helper_mesh = (thing->DerivedFrom(ThingType::SpotLight3D))
+                    ? m_spSpotLight3DMesh : m_spPointLight3DMesh;
+                auto _light{DCast<Light3D>(thing)};
+                auto _transform{_light->GlobalTransform()};
+                _transform.scale *= glm::vec3{0.4f};
+                _fullbright_shader->SetUniform("model_matrix", _transform.model_matrix());
+                _fullbright_shader->SetUniform("current_material.diffuse_color", _light->mColor);
             }
-            if(_enable_3d_rendering and not _camera3d->invalid())
-            {
-                if(ThingFactory::IsDerivedFrom(_type, ThingType::Camera3D))
-                    { DrawCamera3DHelper(_camera3d, DCast<Camera3D>(thing)); }
-                else if(ThingFactory::IsDerivedFrom(_type, EditorPlayer3D::sClassType()))
-                    { DrawPlayer3DHelper(_camera3d, DCast<Actor3D>(thing)); }
-                else if(ThingFactory::IsDerivedFrom(_type, ThingType::Light3D)
-                        and not ThingFactory::IsDerivedFrom(_type, ThingType::DirectionalLight3D))
-                    { DrawLight3DHelper(_camera3d, DCast<Light3D>(thing)); }
-            }
+            else
+                { continue; }
+            for(int i{0}; i < _helper_mesh->SurfaceCount(); ++i)
+                { _renderer_api->DrawIndexed(_helper_mesh->SurfaceGetVertexArray(i)); }
         }
 
+        _fullbright_shader->Unbind();
         RendererAPI::Get()->SetWireframe(_wireframe);
-
         viewport->Detach();
     }
-}
-
-void EditorTheatre::SetEditorViewports(ID in3DViewportUID, ID in2DViewportUID)
-{
-    LockGuard<RMutex> _things_lock{mThingsMutex};
-    if(mViewportIDs.contains(in3DViewportUID))
-    {
-        m_pEditor3DViewport = GetThinker<Viewport>(in3DViewportUID);
-        m_pEditorCamera3D = GetThinker<Camera3D>(m_pEditor3DViewport->CurrentCamera3D());
-    }
-    if(mViewportIDs.contains(in2DViewportUID))
-    {
-        m_pEditor2DViewport = GetThinker<Viewport>(in2DViewportUID);
-        m_pEditorCamera2D = GetThinker<Camera2D>(m_pEditor2DViewport->CurrentCamera2D());
-    }
-}
-
-void EditorTheatre::DrawCamera2DHelper(Shared<Camera2D> inCamera, Shared<Actor2D> inOtherCamera)
-{
-    // if(Console::GetVariable("Editor.nodraw_2d_helpers").int_value)
-        // { return; }
-    // LockGuard<RMutex> _things_lock{mThingsMutex};
-    return;
-}
-
-void EditorTheatre::DrawCamera3DHelper(Shared<Camera3D> inCamera, Shared<Actor3D> inOtherCamera)
-{
-    if(Console::GetVariable("Editor.nodraw_3d_helpers").int_value)
-        { return; }
-    LockGuard<RMutex> _things_lock{mThingsMutex};
-    FAUTO renderer_api{RendererAPI::Get()};
-    auto shader{renderer_api->GetShader(Shaders::Fullbright)};
-    shader->SetUniform("current_material.use_diffuse", 0);
-    shader->SetUniform("current_material.use_specular", 0);
-    shader->SetUniform("current_material.diffuse_color", {200.0f, 80.0f, 255.0f});
-    shader->SetUniform("current_material.alpha", 1.0f);
-    shader->SetUniform("model_matrix", inOtherCamera->GlobalTransform().model_matrix());
-    shader->SetUniform("projection_matrix", inCamera->ProjectionMatrix());
-    shader->SetUniform("debug_output", Console::GetVariable("BlinnPhong.debug_visual").int_value);
-    shader->SetUniform("view_matrix", inCamera->ViewMatrix());
-    shader->SetUniform("view_position", inCamera->GlobalPosition());
-    shader->SetUniform("debug_highlight", inOtherCamera->DebugHighlight());
-    shader->Bind();
-    for(int i{0}; i < m_spCamera3DMesh->SurfaceCount(); ++i)
-        { renderer_api->DrawIndexed(m_spCamera3DMesh->SurfaceGetVertexArray(i)); }
-}
-
-void EditorTheatre::DrawPlayer3DHelper(Shared<Camera3D> inCamera, Shared<Actor3D> inPlayer)
-{
-    if(Console::GetVariable("Editor.nodraw_3d_helpers").int_value)
-        { return; }
-    LockGuard<RMutex> _things_lock{mThingsMutex};
-    FAUTO renderer_api{RendererAPI::Get()};
-    auto shader{renderer_api->GetShader(Shaders::Fullbright)};
-    shader->SetUniform("current_material.use_diffuse", 0);
-    shader->SetUniform("current_material.use_specular", 0);
-    shader->SetUniform("current_material.diffuse_color", glm::vec3{1.0f});
-    shader->SetUniform("current_material.alpha", 1.0f);
-    shader->SetUniform("model_matrix", inPlayer->GlobalTransform().model_matrix());
-    shader->SetUniform("projection_matrix", inCamera->ProjectionMatrix());
-    shader->SetUniform("debug_output", Console::GetVariable("BlinnPhong.debug_visual").int_value);
-    shader->SetUniform("view_matrix", inCamera->ViewMatrix());
-    shader->SetUniform("view_position", inCamera->GlobalPosition());
-    shader->SetUniform("debug_highlight", inPlayer->DebugHighlight());
-    shader->Bind();
-    for(int i{0}; i < m_spPlayerAxis3DMesh->SurfaceCount(); ++i)
-        { renderer_api->DrawIndexed(m_spPlayerAxis3DMesh->SurfaceGetVertexArray(i)); }
-}
-
-void EditorTheatre::DrawLight3DHelper(Shared<Camera3D> inCamera, Shared<Light3D> inLight)
-{
-    LockGuard<RMutex> _things_lock{mThingsMutex};
-    FAUTO renderer_api{RendererAPI::Get()};
-    auto shader{renderer_api->GetShader(Shaders::Fullbright)};
-    auto _transform{inLight->GlobalTransform()};
-    _transform.scale *= glm::vec3{0.4f};
-    shader->SetUniform("current_material.use_diffuse", 0);
-    shader->SetUniform("current_material.use_specular", 0);
-    shader->SetUniform("current_material.diffuse_color", inLight->mColor);
-    shader->SetUniform("current_material.alpha", 1.0f);
-    shader->SetUniform("model_matrix", _transform.model_matrix());
-    shader->SetUniform("projection_matrix", inCamera->ProjectionMatrix());
-    shader->SetUniform("debug_output", Console::GetVariable("BlinnPhong.debug_visual").int_value);
-    shader->SetUniform("view_matrix", inCamera->ViewMatrix());
-    shader->SetUniform("view_position", inCamera->GlobalPosition());
-    shader->SetUniform("debug_highlight", inLight->DebugHighlight());
-    shader->Bind();
-    Shared<Mesh> _mesh{m_spPointLight3DMesh};
-    if(ThingFactory::IsDerivedFrom(inLight->Type(), ThingType::SpotLight3D))
-        { _mesh = m_spSpotLight3DMesh; }
-    for(int i{0}; i < _mesh->SurfaceCount(); ++i)
-        { renderer_api->DrawIndexed(_mesh->SurfaceGetVertexArray(i)); }
 }
--- Editor/theatre/editor_theatre.hpp
+++ Editor/theatre/editor_theatre.hpp
@@ -3,6 +3,7 @@
 
 #include <Nostalgia/Nostalgia.hpp>
 #include <Nostalgia/theatre/theatre.hpp>
+#include <Nostalgia/things/thing_factory.hpp>
 #include <Nostalgia/things/thinkers/viewport.hpp>
 #include <Nostalgia/things/thinkers/3d/camera_3d.hpp>
 #include <Nostalgia/things/thinkers/2d/camera_2d.hpp>
@@ -17,23 +18,17 @@ public:
     virtual void Input(InputEvent*) final {}
     virtual void Draw() final;
 
-    void SetEditorViewports(ID in3DViewportUID, ID in2DViewportUID);
-
-    Shared<Camera3D> m_pEditorCamera3D{MakeShared<Camera3D>("Editor-Camera3D")};
-    Shared<Camera2D> m_pEditorCamera2D{MakeShared<Camera2D>("Editor-Camera2D")};
-    Shared<Viewport> m_pEditor3DViewport{MakeShared<Viewport>("Editor3D-Viewport")},
-        m_pEditor2DViewport{MakeShared<Viewport>("Editor2D-Viewport")};
+    Shared<Camera3D> m_pEditorCamera3D{ThingFactory::MakeThing<Camera3D>("Editor-Camera3D")};
+    Shared<Camera2D> m_pEditorCamera2D{ThingFactory::MakeThing<Camera2D>("Editor-Camera2D")};
+    Shared<Viewport> m_pEditor3DViewport{ThingFactory::MakeThing<Viewport>("Editor3D-Viewport")},
+        m_pEditor2DViewport{ThingFactory::MakeThing<Viewport>("Editor2D-Viewport")};
 
 private:
     inline static Shared<Mesh> m_spCamera3DMesh{nullptr},
         m_spPointLight3DMesh{nullptr},
         m_spSpotLight3DMesh{nullptr},
-        m_spPlayerAxis3DMesh{nullptr};
-
-    void DrawCamera2DHelper(Shared<Camera2D>, Shared<Actor2D>);
-    void DrawCamera3DHelper(Shared<Camera3D>, Shared<Actor3D>);
-    void DrawPlayer3DHelper(Shared<Camera3D>, Shared<Actor3D>);
-    void DrawLight3DHelper(Shared<Camera3D>, Shared<Light3D>);
+        m_sp3DAxisMesh{nullptr},
+        m_spPlayer3DMesh{nullptr};
 };
 
 #endif // EDITOR_THEATRE_H
